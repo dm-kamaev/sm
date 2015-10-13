e457549c0bca71d91b8c0843e75ebf232d362bbd
Nikita Gubchenko <n.gubchenko@gmail.com>
2015-10-13 14:51:16 +0300

    BP-403: Fixed soy compilation

--- dev/blocks/l-school/l-school.js
+++ dev/blocks/l-school/l-school.js
@@ -1,2 +1,5 @@
-document.write(bp.lSchool.Template.base());
-// console.log('Hello, soy!');
+document.write(bp.lSchool.Template.base({params:{
+    image: "123.jpg",
+    text:"text",
+    buttonAlt:"buttonsdsf"
+}}));
