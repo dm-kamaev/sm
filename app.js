a7b2e9aa71ffca8670773c31fd2c82c1dc9baf84
Kirill Sakhanenko <fourslashw@gmail.com>
2015-10-13 19:45:58 +0300

    BP-396 working page skeleton

--- app.js
+++ app.js
@@ -11,8 +11,38 @@ const CONFIG = {
 };
 
 const LANDING_TEMPLATE = {
-    template: 'mel.lschools.layout',
-    params: {}
+    template: 'bp.lSchool.Template.base',
+    arghs: {
+        params:{
+            data:{
+                schoolName: "Имя школы",
+                schoolDescr: "Много много текста",
+                directorName: "Имя директора",
+                classes:"строка классов обучения",
+                social:[
+                    {name:"Твиттер", href:"#"},
+                    {name:"Вконтакте", href:"#"},
+                    {name:"Одноклассники", href:"#"},
+                    {name:"Фейсбук", href:"#"}
+                ],
+                sites:[
+                    {name:"Перейти на сайт школы", href:"#", link:"safasf.com"},
+                    {name:"Страница образования на сайте гор москвы", href:"#", link:"safasf.com"}
+                ],
+                contacts:{
+                    address:[
+                        {title:"", description:"dsaggadgsadgas"},
+                        {title:"asgasgdda", description:"dsaggadgsadsagdgas"},
+                        {title:"sagasg", description:"dsaggadgsadasgaggas"}
+                    ],
+                    phones:[
+                        {phone:"+7-909-673-96-55"},
+                        {phone:"+8-909-673-96-56"}
+                    ]
+                }
+            }
+        }
+    }
 };
 // soynode.setOptions({
 //     allowDinamicRecompile: true
@@ -20,7 +50,7 @@ const LANDING_TEMPLATE = {
 
 const sendCompiledTemplate = (action, templateObj) =>
     soynode.loadCompiledTemplateFiles(path.join(__dirname + '/tmp/templates.js'), (err) =>
-        err ? console.log('Compilation failed: ' + err) : action(soynode.render(templateObj.template, templateObj.params))
+        err ? console.log('Compilation failed: ' + err) : action(soynode.render(templateObj.template, templateObj.arghs))
 );
 
 
