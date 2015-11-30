60ee2b9ebbf1b065799858259ec570d75bfa1143
Sakhanenko Kirill <fourslashw@gmail.com>
2015-11-30 18:44:20 +0400

    BP-547 search controller

--- api/modules/school/services/search.js
+++ api/modules/school/services/search.js
@@ -3,6 +3,7 @@ var await = require('asyncawait/await');
 var models = require.main.require('./app/components/models').all;
 var services = require.main.require('./app/components/services').all;
 var sequelizeInclude = require.main.require('./api/components/sequelizeInclude');  
+var colors = require('colors');
 
 exports.name = 'search';
 
@@ -14,6 +15,62 @@ exports.getSchoolRecords = async (function(id) {
     }));
 });
 
+/**
+ * @public
+ */
+exports.searchSchool = async (params => {
+    var searchParams = params.searchParams,
+        includeParams = {
+           searchTemplates: {
+               where: {
+                   $or: []
+               }
+           }   
+        },
+	    whereParams = {};
+    if (searchParams.name) {
+	    whereParams.$or = [
+		{
+			name: {$like: '%' + searchParams.name + '%'} 
+		}, {
+			fullName:{$like: '%' + searchParams.name + '%'} 
+		}
+    	];
+    }
+
+    if (searchParams.classes && searchParams.classes.length) {
+	    whereParams.educationInterval = { 
+		    $contains: searchParams.classes
+    	};
+    }
+
+    if (searchParams.schoolType && searchParams.schoolType.length) {
+	    whereParams.schoolType = {
+		    $or:[]  
+    	};
+        searchParams.schoolType.forEach((item) => { 
+	        whereParams.schoolType.$or.push(item);
+   	    });
+    }
+    
+    if (searchParams.gia) {
+        includeParams.searchTemplates.where.$or.push({
+                type: 'gia',
+                values: {
+                    $contains: searchParams.gia
+                }       
+            });
+        } 
+    
+    var params = {
+        where: whereParams
+    };
+    if (Object.keys(includeParams))
+        params.include = sequelizeInclude(includeParams);
+    var results = await (models.School.findAll(params));
+    console.log('Found: ', colors.green(results.length));
+    return JSON.stringify(results);
+});
 
 exports.addGia = async(function(schoolId, values) {
     await (models.Search.create({
