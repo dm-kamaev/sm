dce3b0554dd0b364a6c2582db62df5c33d7d6505
Sakhanenko Kirill <fourslashw@gmail.com>
2015-12-01 13:19:33 +0400

    BP-547 vim tabs

--- api/modules/school/services/search.js
+++ api/modules/school/services/search.js
@@ -8,75 +8,75 @@ var colors = require('colors');
 exports.name = 'search';
 
 exports.getSchoolRecords = async (function(id) {
-	return await (models.SearchData.findAll({
-		where: {
-			schoolId: id
-		}
-	}));
+        return await (models.SearchData.findAll({
+                where: {
+                        schoolId: id
+                }
+        }));
 });
 
 /**
  * @public
  */
 exports.searchSchool = async (params => {
-	var searchParams = params.searchParams,
-		includeParams = {
-		   searchData: {
-			   where: {
-				   $or: []
-			   }
-		   }   
-		},
-		whereParams = {};
-	if (searchParams.name) {
-		whereParams.$or = [
-		{
-			name: {$like: '%' + searchParams.name + '%'} 
-		}, {
-			fullName:{$like: '%' + searchParams.name + '%'} 
-		}
-		];
-	}
+        var searchParams = params.searchParams,
+                includeParams = {
+                   searchData: {
+                           where: {
+                                   $or: []
+                           }
+                   }   
+                },
+                whereParams = {};
+        if (searchParams.name) {
+                whereParams.$or = [
+                {
+                        name: {$like: '%' + searchParams.name + '%'} 
+                }, {
+                        fullName:{$like: '%' + searchParams.name + '%'} 
+                }
+                ];
+        }
 
-	if (searchParams.classes && searchParams.classes.length) {
-		whereParams.educationInterval = { 
-			$contains: searchParams.classes
-		};
-	}
+        if (searchParams.classes && searchParams.classes.length) {
+                whereParams.educationInterval = { 
+                        $contains: searchParams.classes
+                };
+        }
 
-	if (searchParams.schoolType && searchParams.schoolType.length) {
-		whereParams.schoolType = {
-			$or:[]  
-		};
-		searchParams.schoolType.forEach((item) => { 
-			whereParams.schoolType.$or.push(item);
-		});
-	}
-	
-	if (searchParams.gia) {
-		includeParams.searchData.where.$or.push({
-				type: 'gia',
-				values: {
-					$contains: searchParams.gia
-				}       
-			});
-		} 
-	
-	var params = {
-		where: whereParams
-	};
-	if (Object.keys(includeParams))
-		params.include = sequelizeInclude(includeParams);
-	var results = await (models.School.findAll(params));
-	console.log('Found: ', colors.green(results.length));
-	return JSON.stringify(results);
+        if (searchParams.schoolType && searchParams.schoolType.length) {
+                whereParams.schoolType = {
+                        $or:[]  
+                };
+                searchParams.schoolType.forEach((item) => { 
+                        whereParams.schoolType.$or.push(item);
+                });
+        }
+        
+        if (searchParams.gia) {
+                includeParams.searchData.where.$or.push({
+                                type: 'gia',
+                                values: {
+                                        $contains: searchParams.gia
+                                }       
+                        });
+                } 
+        
+        var params = {
+                where: whereParams
+        };
+        if (Object.keys(includeParams))
+                params.include = sequelizeInclude(includeParams);
+        var results = await (models.School.findAll(params));
+        console.log('Found: ', colors.green(results.length));
+        return JSON.stringify(results);
 });
 
 exports.addGia = async(function(schoolId, values) {
-	await (models.SearchData.create({
-		schoolId: schoolId,
-		type: 'gia',
-		values: values
-	}))
+        await (models.SearchData.create({
+                schoolId: schoolId,
+                type: 'gia',
+                values: values
+        }))
 })
 
