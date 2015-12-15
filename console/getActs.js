e8d8dddb410b7d580e3e569ab2d910c049a0224d
dmedvedev <juggernay@yahoo.com>
2015-12-15 19:46:44 +0300

    BP-584 refactoring and improved speed

--- console/getActs.js
+++ console/getActs.js
@@ -38,7 +38,7 @@ var countActs = function() {
     return request(options);
 };
 
-var getActivities = function(params) {
+var getActivitiesSet = function(params) {
     var path = '/v1/datasets/746/rows?';
     for (var prop in params) {
         path += '$' + prop + '=' + params[prop] + '&';
@@ -51,66 +51,82 @@ var getActivities = function(params) {
     return request(options);
 };
 
-var ourSchools = {}; // test
 var nameSchools = {}; // test
 
-var fillActsDB = function(schools, activities) {
-    var foundCount = 0;
-    for (var i = 0, actsLength = activities.length; i < actsLength; i++) {
+var findActivityMatches = function(schools, activityNumber, activityName) {
+    var ourSchools = {};
+    for (var i = 0, schoolsLength = schools.length; i < schoolsLength; i++) {
+        if (findMatch(schools[i], activityNumber, activityName)) {
+            ourSchools[schools[i].id] = schools[i].fullName;
+            break;
+        }
+    }
+    return ourSchools;
+};
 
+var mergeObjects = function (obj1,obj2) {
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+};
+
+var createParsedSchools = function(schools) {
+    var parsedSchools = [];
+    for (var i = 0, schoolsLength = schools.length; i < schoolsLength; i++) {
+        var parsedSchoolNumber = schools[i].fullName
+            .slice(schools[i].fullName.lastIndexOf('№ ') + 2)
+            .match(/\d+/g),
+            schoolNumber = parsedSchoolNumber ? parsedSchoolNumber[0] : NaN,
+            parsedSchoolName = schools[i].fullName.match(/(?!.*[«]).+(?=[»])/g),
+            schoolName = parsedSchoolName ? parsedSchoolName[0].toLowerCase() : null;
+        parsedSchools.push({
+            'number': schoolNumber,
+            'name': schoolName,
+            'id': schools[i].id,
+            'fullName': schools[i].fullName
+        });
+    }
+    return parsedSchools;
+};
+
+var createParsedActivities = function(activities) {
+    var parsedActivities = [];
+    for (var i = 0, actLength = activities.length; i < actLength; i++) {
         var activityNumber = activities[i].Cells.nomer.trim(),
             parsedActivityName = activities[i].Cells.poln_name.match(/"(.*?)"/),
             activityName = parsedActivityName ?
-                parsedActivityName[0].slice(1, -1).toLowerCase() :
-                null;
-
-        if (activities[i].Cells.name_grupp.length <= 255) {
-            for (var j = 0, schoolsLength = schools.length; j < schoolsLength; j++) {
-                // var parsedSchoolNumber = schools[j].fullName
-                //     .slice(schools[j].fullName.lastIndexOf('№ ') + 2)
-                //     .match(/\d+/g),
-                //     schoolNumber = parsedSchoolNumber ? parsedSchoolNumber[0] : NaN,
-                //     parsedSchoolName = schools[j].fullName.match(/(?!.*[«]).+(?=[»])/g),
-                //     schoolName = parsedSchoolName ? parsedSchoolName[0].toLowerCase() : null;
-                //
-                // if (schoolNumber === activityNumber) {
-                //     //addActivity(activity, school);
-                //     ourSchools.push(schools[j].fullName); // test
-                //     isFound = true;
-                // } else if (activityNumber === '0' && typeof schoolName === 'string' && schoolName === activityName) {
-                //     //addActivity(activity, school);
-                //     nameSchools.push(schools[j].fullName);
-                //     ourSchools.push(schools[j].fullName);
-                //     isFound = true;
-                // }
-                if (findMatch(schools[j], activityNumber, activityName)) {
-                    // addActivity(activities[i], schools[j]);
-                    foundCount++;
-                    break;
-                }
-            }
-        }
+                parsedActivityName[0].slice(1, -1).toLowerCase() : null;
+        parsedActivities.push({
+            'number': activityNumber,
+            'name': activityName,
+            'groupLength': activities[i].Cells.name_grupp.length
+        });
     }
-    return foundCount;
+    return parsedActivities;
+}
+
+var fillActsDB = function(schools, activities) {
+    var ourSchools = {};
+    for (var i = 0, actsLength = activities.length; i < actsLength; i++) {
+        var ourSetSchools = {};
+        if (activities[i].groupLength <= 255) {
+            ourSetSchools = findActivityMatches(schools, activities[i].number, activities[i].name);
+        }
+    ourSchools = mergeObjects(ourSchools, ourSetSchools);
+        console.log('Act\'s processing: ' + i);
+        console.log('Our schools found: ' + Object.keys(ourSchools).length);
+    }
+    return ourSchools;
 };
 
 var findMatch = function(school, actSchoolNum, actSchoolName) {
-    var parsedSchoolNumber = school.fullName
-        .slice(school.fullName.lastIndexOf('№ ') + 2)
-        .match(/\d+/g),
-        schoolNumber = parsedSchoolNumber ? parsedSchoolNumber[0] : NaN,
-        parsedSchoolName = school.fullName.match(/(?!.*[«]).+(?=[»])/g),
-        schoolName = parsedSchoolName ? parsedSchoolName[0].toLowerCase() : null;
-
-    if (schoolNumber === actSchoolNum) {
-        //ourSchools.push(school.fullName); // test
-        ourSchools[school.id] = school.fullName;
+    if (school.number === actSchoolNum) {
         return true;
-    } else if (actSchoolNum === '0' && typeof schoolName === 'string' && schoolName === actSchoolName) {
-        // nameSchools.push(school.fullName);
+    } else if (actSchoolNum === '0' &&
+            typeof school.name === 'string' &&
+            school.name === actSchoolName) {
         nameSchools[school.id] = school.fullName;
-        //ourSchools.push(school.fullName);
-        ourSchools[school.id] = school.fullName;
         return true;
     } else {
         return false;
@@ -128,32 +144,22 @@ var addActivity = function(activity, school) {
     });
 };
 
-var getUnique = function(array) {
-    var u = {},
-        a = [];
-    for(var i = 0, l = array.length; i < l; ++i) {
-        if(u.hasOwnProperty(array[i])) {
-            continue;
-        }
-        a.push(array[i]);
-        u[array[i]] = 1;
-    }
-    return a;
-};
-
 var notFoundSchools = function(allSchools, findedSchools) {
     var missingSchools = [];
     allSchools.forEach(function(school) {
+        var isFound = false;
         for (var id in findedSchools) {
-            if (findedSchools.hasOwnProperty[id]) {
-                if (school.fullName !== findedSchools[id]) {
-                    missingSchools.push({
-                        id: school.id,
-                        name: school.name,
-                        fullName: school.fullName
-                    });
-                }
+            if (school.fullName === findedSchools[id]) {
+                isFound = true;
+                break;
             }
+        }
+        if (!isFound) {
+            missingSchools.push({
+                id: school.id,
+                name: school.name,
+                fullName: school.fullName
+            });
         }
     });
     return missingSchools;
@@ -163,7 +169,8 @@ var getAllActivities = async(function() {
     var actCount = await(countActs()),
         activities = [];
     for(var top = 250, skip = 0; skip <= actCount; skip += 250) {
-        var actsSet = await(getActivities({
+        console.log('Current set: ' + skip);
+        var actsSet = await(getActivitiesSet({
             top: top,
             skip: skip
         }));
@@ -172,61 +179,55 @@ var getAllActivities = async(function() {
     return activities;
 });
 
-var start = async(function() {
-    var schools = await(services.school.listInstances()),
-        actCount = await(countActs()),
-        foundActsCount = 0,
-        ourNotFoundSchools = [],
-        activities = [];
+var downloadActivities = async(function(fileName) {
+    var acts = await(getAllActivities());
+    fs.writeFileSync(REPORT_PATH + fileName, JSON.stringify(acts));
+    return acts;
+});
 
-    for(var top = 250, skip = 0; skip <= actCount; skip += 250) {
-        var activities = await(getActivities({
-            top: top,
-            skip: skip
-        }));
-        foundActsCount += fillActsDB(schools, activities);
-        //ourSchools = getUnique(ourSchools); // test
-        //nameSchools = getUnique(nameSchools);
-        console.log('Set of data: ' + skip);
-        console.log('Our schools matched: ' + Object.keys(ourSchools).length); // test
-        console.log('Found act\'s: ' + foundActsCount);
-        console.log(nameSchools);
+var openActivitiesFile = function(fileName) {
+    var acts,
+        filePath = REPORT_PATH + fileName;
+    try {
+        fs.statSync(filePath).isFile();
+        console.log('Loading activities from ' + filePath);
+        acts = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } catch(err) {
+        console.log(filePath + ' not found, downloading activities...');
+        acts = downloadActivities(fileName);
+    } finally {
+        return acts;
     }
+};
 
-    ourNotFoundSchools = notFoundSchools(schools, ourSchools);
-    // schools.forEach(function(school) {
-    //     for (var id in ourSchools) {
-    //         if (ourSchools.hasOwnProperty[id]) {
-    //             if (school.fullName !== ourSchools[id]) {
-    //                 ourNotFoundSchools.push({
-    //                     id: school.id,
-    //                     name: school.name,
-    //                     fullName: school.fullName
-    //                 });
-    //             }
-    //         }
-    //     }
-    //     // ourSchools.forEach(function(ourSchool) {
-    //     //     if (school.fullName === ourSchool) {
-    //     //         flag = true;
-    //     //     }
-    //     // });
-    //     // if (!flag) {
-    //     //     ourNotFoundSchools.push({
-    //     //         id: school.id,
-    //     //         name: school.name,
-    //     //         fullName: school.fullName
-    //     //     });
-    //     // }
-    // });
+var start = async(function() {
+    console.time("Matching");
+    var schools = await(services.school.listInstances()),
+        foundSchools = {},
+        ourNotFoundSchools = [],
+        activities = await(openActivitiesFile('activities.json')),
+        parsedSchools = createParsedSchools(schools),
+        parsedActivities = createParsedActivities(activities);
+
     mkdirp(REPORT_PATH, function(err) {
         if (err) {
             return;
         }
-        fs.writeFileSync(REPORT_PATH + 'notFindOurSchools.json', JSON.stringify(ourNotFoundSchools));
     });
+
+    foundSchools = await(fillActsDB(parsedSchools, parsedActivities));
+
+    console.log('Total schools: ' + schools.length);
+    console.log('Total act\'s: ' + activities.length);
+    console.log('Our schools matched: ' + Object.keys(foundSchools).length);
+
+    ourNotFoundSchools = notFoundSchools(schools, foundSchools);
+    fs.writeFileSync(REPORT_PATH + 'notFindOurSchools.json', JSON.stringify(ourNotFoundSchools));
     console.log('Schools\' count haven\'t been found: ' + ourNotFoundSchools.length);
-    //console.log(nameSchools);
+
+    console.log('schools found with no number: ');
+    console.log(nameSchools);
+    console.timeEnd("Matching");
 });
 
 commander
