e9a70952adecf8f4fb79451cc0cefd5bb2c95450
Sakhanenko Kirill <fourslashw@gmail.com>
2015-12-30 15:55:23 +0400

    BP-694 department refactoring

--- api/modules/geo/migrations/20151119120144-department.js
+++ api/modules/geo/migrations/20151119120144-department.js
@@ -1,10 +1,5 @@
 'use strict';
-
-const path = require('path');
-const async = require('asyncawait/async');
-const ModelArchiver = require('../../console/modules/modelArchiver/ModelArchiver.js') ;
-const Department = require('../../api/modules/geo/models/department');
-const dataFolder = path.join(__dirname, '../../api/modules/geo/migrations');
+const departmentStage = require('../../api/modules/geo/enums/departmentStage');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
@@ -16,16 +11,13 @@ module.exports = {
             type: Sequelize.INTEGER
         },
         name: Sequelize.STRING,
-        stage: { // 'Дошкольное образование', 'Начальное образование', 'Основное и среднее', 'Дополнительное  образование', 'Профессиональное образование'
+        stage: { 
             type: Sequelize.ENUM,
-            values: ['Дошкольное образование', 'Начальное образование', 'Основное и среднее', 'Дополнительное образование', 'Профессиональное образование']
+            values: departmentStage.toArray()
         },
         created_at: Sequelize.DATE,
         updated_at: Sequelize.DATE
-     }).then(async(function() {
-        var archiver = new ModelArchiver(Department, dataFolder);
-        archiver.load();
-    }));
+    });
   },
   down: function (queryInterface, Sequelize) {
     return queryInterface.dropTable('department');
