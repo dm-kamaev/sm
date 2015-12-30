122c1f484ef431313bcbf714dc24b7f638eeaae6
Andrey Baikov <bboyair@mail.ru>
2015-12-30 14:41:29 +0300

    BP-696: encoding bug fixed

--- app/modules/school/controllers/schoolController.js
+++ app/modules/school/controllers/schoolController.js
@@ -33,6 +33,13 @@ exports.list = async (function(req, res) {
 
     var schools = schoolView.list(results[0]);
     var filters = schoolView.filters(results[1]);
+    var searchText;
+
+    try{
+        searchText = decodeURIComponent(req.query.name);
+    } catch(e) {
+        searchText = req.query.name;
+    }
 
     var params = {
         params: {
@@ -44,7 +51,7 @@ exports.list = async (function(req, res) {
                 }
             },
             searchText: req.query.name ?
-                services.school.convertWinToUnicode(req.query.name) : '',
+                searchText : '',
             templates: {
                 search: '{{ name }}',
                 item: '{{ name }}',
