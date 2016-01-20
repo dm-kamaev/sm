add97b17ab0816cfc45167353ad17fe481da2d94
dmedvedev <juggernay@yahoo.com>
2016-01-20 17:08:46 +0300

    BP-644 speialized classes made and data migrations fix

--- api/modules/geo/migrations/20160119125633-new-geo.js
+++ api/modules/geo/migrations/20160119125633-new-geo.js
@@ -4,10 +4,12 @@ const ModelArchiver = require('../../console/modules/modelArchiver/ModelArchiver
 const Area = require('../../api/modules/geo/models/area');
 const Address = require('../../api/modules/geo/models/address');
 const Department = require('../../api/modules/geo/models/department');
+const AddressMetro = require('../../api/modules/geo/models/addressMetro')
 const dataFolder = path.join(__dirname, '../../api/modules/geo/migrations');
 const archive1 = '20160119125633-new-geo_1.tar.gz';
 const archive2 = '20160119125633-new-geo_2.tar.gz';
 const archive3 = '20160119125633-new-geo_3.tar.gz';
+const archive4 = '20160119125633-new-geo_4.tar.gz';
 const await = require('asyncawait/await');
 const async = require('asyncawait/async');
 module.exports = {
@@ -18,6 +20,8 @@ module.exports = {
         await(archiver2.load());
         var archiver3 = new ModelArchiver(Department, dataFolder, null, archive3);
         await(archiver3.load());
+        var archiver4 = new ModelArchiver(AddressMetro, dataFolder, null, archive4);
+        await(archiver4.load());
     }),
     down: function () {
         return null;
