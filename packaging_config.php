37e59a983de96a4cede23ef2909e697d7dcf6440
Mikhail Elfimov <melfimov@yopolis.ru>
2016-01-21 22:09:31 +0300

    BP-723 deployment

---
+++ packaging_config.php
@@ -0,0 +1,33 @@
+// Build package using https://github.com/liip/packaging
+// run packaging bin/make-deb
+
+defined('LIB_DIR') || define('LIB_DIR', 'node_modules/deployment');
+$project_release_number = getenv('packaging_release_number');
+
+$configure = [
+    'packagename' => 'school-market',
+    'arch' => 'all',
+    'version' => "1.$project_release_number",
+    'packagetype' => 'deb',
+
+    'tmpdir' => '/tmp',
+    'templatedir' => 'templates',
+    'postinst' => 'templates/postinstall',
+    'preinst' => '',
+    'postrm' => '',
+    'prerm' => 'templates/prerm',
+];
+
+$filemapping = [
+    'opt/@PACKAGENAME@/@VERSION@' => [
+        '*',
+
+        // files and folders to exclude from package
+        '- /packaging',
+        '- deployment/make-deb',
+        '- deployment/deploy',
+        '- /node_modules/deployment',
+        '- packaging_config.php',
+    ],
+    'etc/init.d/@PACKAGENAME@-node' => 'templates/@PACKAGENAME@-node',
+];
