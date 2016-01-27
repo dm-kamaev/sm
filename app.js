53bd3d38360105f062d1b548fa70e4be94bd4ec3
Andrey Baikov <bboyair@mail.ru>
2016-01-27 14:52:15 +0300

    BP-640: gulp refactor

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/api-debug', express.static(path.join(__dirname, '/api-debug')));
 
 
 soy.init(
-    path.join(__dirname, '/node_modules/frobl/'),
+    path.join(__dirname, '/tmp/compiledServerSoy/server.soy.concat.js'),
     function() {
         app.listen(CONFIG.PORT, function() {
             console.log('Running at port ' + CONFIG.PORT)
