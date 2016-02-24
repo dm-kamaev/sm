ccb2a2ef8577b6cece5679cee2f8433e48b38316
anedashkovsky <texnik88@gmail.com>
2016-02-24 18:00:23 +0300

    BP-789: comment deletind in migration added

--- api/modules/user/migrations/20160220124644-comment-user-data.js
+++ api/modules/user/migrations/20160220124644-comment-user-data.js
@@ -1,12 +1,20 @@
 'use strict';
 
+const path = require('path');
+const await = require('asyncawait/await');
+const async = require('asyncawait/async');
+const sequelize = require('../../app/components/db');
+const SqlHelper = require('../../console/modules/sqlHelper/SqlHelper.js');
+
 var userType = require('../../api/modules/user/enums/userType');
 
 module.exports = {
-    up: function (queryInterface, Sequelize) {
-        return queryInterface.removeColumn('comment', 'user_type')
-            .then(function () {
-                return queryInterface.createTable('user_data', {
+    up: async(function (queryInterface, Sequelize) {
+            resetData();
+
+            queryInterface.removeColumn('comment', 'user_type');
+
+            await( queryInterface.createTable('user_data', {
                     id: {
                         allowNull: false,
                         autoIncrement: true,
@@ -26,25 +34,22 @@ module.exports = {
                     },
                     created_at: Sequelize.DATE,
                     updated_at: Sequelize.DATE
-                }).then(function () {
-                    return queryInterface.addColumn('comment', 'user_data_id', {
-                        type: Sequelize.INTEGER,
-                        references: {
-                            model: 'user_data',
-                            key: 'id'
-                        }
-                    });
-                }).then(function () {
-                    return queryInterface.addColumn('rating', 'user_data_id', {
-                        type: Sequelize.INTEGER,
-                        references: {
-                            model: 'user_data',
-                            key: 'id'
-                        }
-                    });
-                });
+            }) );
+            queryInterface.addColumn('comment', 'user_data_id', {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'user_data',
+                    key: 'id'
+                }
             });
-    },
+            queryInterface.addColumn('rating', 'user_data_id', {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'user_data',
+                    key: 'id'
+                }
+            });
+    }),
     down: function (queryInterface) {
         return queryInterface.removeColumn('comment', 'user_data_id')
             .then(function() {
@@ -55,4 +60,21 @@ module.exports = {
     }
 };
 
-
+var resetData =  function() {
+    var resetColumns = 'UPDATE school \
+        SET score=NULL, \
+        comment_group_id=NULL, \
+        total_score=0, \
+        score_count=NULL, \
+        review_count=NULL \
+        WHERE comment_group_id <> 0;';
+    await(SqlHelper.resetTable('comment'));
+    await(SqlHelper.resetTable('rating'));
+    await(sequelize.query(
+        resetColumns,
+        {
+            type: sequelize.QueryTypes.UPDATE
+        }
+    ));
+    await(SqlHelper.resetTable('comment_group'));
+};
