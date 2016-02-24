5873e4ec8d44ef08ee706c3384b36fd6b5b58e22
Andrey Baikov <bboyair@mail.ru>
2016-02-24 11:46:25 +0300

    BP-657: fixes

--- api/modules/study/views/olimpResultView.js
+++ api/modules/study/views/olimpResultView.js
@@ -29,16 +29,16 @@ OlimpResultView.prototype.transformResults = function(results) {
         res[year][subject] = res[year][subject] || {};
 
         if (schoolClass > 8) {
-            res[year][subject].top = res[year][subject].top || {};
+            res[year][subject].bottom = res[year][subject].bottom || {};
 
             if (item.status == 'призер') {
-                res[year][subject].top.awardees =
-                    res[year][subject].top.awardees ?
-                        res[year][subject].top.awardees + 1 : 1;
+                res[year][subject].bottom.awardees =
+                    res[year][subject].bottom.awardees ?
+                        res[year][subject].bottom.awardees + 1 : 1;
             } else if (item.status == 'победитель') {
-                res[year][subject].top.winners =
-                    res[year][subject].top.winners ?
-                        res[year][subject].top.winners + 1 : 1;
+                res[year][subject].bottom.winners =
+                    res[year][subject].bottom.winners ?
+                        res[year][subject].bottom.winners + 1 : 1;
             }
         } else if (schoolClass > 4) {
             res[year][subject].middle = res[year][subject].middle || {};
@@ -53,16 +53,16 @@ OlimpResultView.prototype.transformResults = function(results) {
                         res[year][subject].middle.winners + 1 : 1;
             }
         } else {
-            res[year][subject].bottom = res[year][subject].bottom || {};
+            res[year][subject].top = res[year][subject].top || {};
 
             if (item.status == 'призер') {
-                res[year][subject].bottom.awardees =
-                    res[year][subject].bottom.awardees ?
-                        res[year][subject].bottom.awardees + 1 : 1;
+                res[year][subject].top.awardees =
+                    res[year][subject].top.awardees ?
+                        res[year][subject].top.awardees + 1 : 1;
             } else if (item.status == 'победитель') {
-                res[year][subject].bottom.winners =
-                    res[year][subject].bottom.winners ?
-                        res[year][subject].bottom.winners + 1 : 1;
+                res[year][subject].top.winners =
+                    res[year][subject].top.winners ?
+                        res[year][subject].top.winners + 1 : 1;
             }
         }
     });
