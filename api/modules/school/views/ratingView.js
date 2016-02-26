3b499603e63efdeb3fcb167af48dd3992ad015ca
Pavel Slotinsky <pslotinsky@yopolis.ru>
2016-02-26 20:19:27 +0300

    BP-806: Top 100 Russia -> Top 100 Moscow

--- api/modules/school/views/ratingView.js
+++ api/modules/school/views/ratingView.js
@@ -36,7 +36,7 @@ ratingView.ratingSchoolView = function(rankMel, rankDogm) {
 ratingView.ratingResultView = function(rankDogm) {
     var ratings = [];
     if (checkPlace(rankDogm)) {
-        ratings.push('100 лучших школ России');
+        ratings.push('100 лучших школ Москвы');
     }
     return ratings;
 };
