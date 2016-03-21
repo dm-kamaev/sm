fe0e0a4c32b7f836c447d5dbb82bbf81928803d6
anedashkovsky <texnik88@gmail.com>
2016-03-21 19:10:18 +0300

    BP-947: add view for feedback, fix themes in modal, add mail service

---
+++ api/modules/feedback/views/feedbackView.js
@@ -0,0 +1,50 @@
+var feedbackView = {};
+
+/**
+ * @param {{
+ *     type: string
+ * }} params
+ */
+feedbackView.getLetterTheme = function(params) {
+    var letterTheme = '';
+
+    switch (params.type) {
+        case 'opinion':
+            letterTheme = 'Ваше мнение';
+            break;
+        case 'mistake':
+            letterTheme = 'Сообщить о неточности';
+            break;
+    }
+
+    return letterTheme;
+};
+
+/**
+ * Send recieved feedback message to email
+ * @param {{
+ *     name: string,
+ *     email: string,
+ *     theme: string,
+ *     text: string,
+ *     url: ?string
+ * }} params
+ * @type {{name: string}}
+ */
+feedbackView.getLetterBody = function(params) {
+    var letterBody = '';
+
+    letterBody += 'Имя отправителя: ' + params.name + '\n';
+    letterBody += 'email отправителя: ' + params.email + '\n';
+    letterBody += 'Тема: ' + params.theme + '\n';
+
+    if (params.url) {
+        letterBody += 'Url: ' + params.url + '\n';
+    }
+
+    letterBody += 'Сообщение: ' + params.text + '\n';
+
+    return letterBody;
+};
+
+module.exports = feedbackView;
