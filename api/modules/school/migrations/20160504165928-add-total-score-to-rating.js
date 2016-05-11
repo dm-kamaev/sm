8f6072a66565a345f4c31cb0d31eb7355cddfca1
Alexander Nedashkovsky <texnik88@gmail.com>
2016-05-11 18:41:34 +0300

    BP-1114: migrations fix

--- api/modules/school/migrations/20160504165928-add-total-score-to-rating.js
+++ api/modules/school/migrations/20160504165928-add-total-score-to-rating.js
@@ -7,9 +7,12 @@ const sequelize = require('../../app/components/db');
 
 module.exports  = {
     up: async(function(queryInterface, Sequelize) {
-        return await(queryInterface.addColumn('rating', 'total_score', {
+
+        try{await(queryInterface.addColumn('rating', 'total_score', {
             type: Sequelize.FLOAT
-        }));
+        }))}
+        catch (error) {
+        };
     }),
     down: async(function (queryInterface, Sequelize) {
         return await(queryInterface.removeColumn('rating', 'total_score'));
