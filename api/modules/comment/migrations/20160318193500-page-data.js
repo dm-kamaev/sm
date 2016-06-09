49d32d88aaecc50f07dbc17e4c1b62115a019966
Dmitry Medvedev <juggernay@yahoo.com>
2016-06-09 15:35:54 +0300

    BP-1225 backward compatibility for page table

--- api/modules/comment/migrations/20160318193500-page-data.js
+++ api/modules/comment/migrations/20160318193500-page-data.js
@@ -12,15 +12,12 @@ const TABLE = 'page',
 
 module.exports = {
     up: async(function() {
-        var pages = await(sequelize.query('SELECT COUNT(*) FROM page;'));
-        if (!pages) {
-            var dir = path.join(__dirname, '../../api/modules/entity/migrations'),
-                file = '20160603133300-page-data.tar.gz',
-                filePath = path.join(dir, file),
-                archiver = new Archiver(filePath);
+        var dir = path.join(__dirname, '../../api/modules/comment/migrations'),
+            file = '20160318193500-page-data.tar.gz',
+            filePath = path.join(dir, file),
+            archiver = new Archiver(filePath);
 
-            archiver.copyToDb(TABLE, DELIMITER);
-        }        
+        archiver.copyToDb(TABLE, DELIMITER);
     }),
     down: async(function(queryInterface) {
 
