b943b766647e102cb5ee979cc48040c9fa02a81d
Dmitry Medvedev <juggernay@yahoo.com>
2016-06-09 15:33:36 +0300

    BP-1225 alias_backlog fix

---
+++ api/modules/school/migrations/20160609152800-drop-school-url.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return queryInterface.dropTable('school_url');
+    },
+    down: function (queryInterface) {
+        return queryInterface.createTable('school_url', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            school_id: {
+                onDelete: 'cascade',
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'school',
+                    key: 'id'
+                }
+            },
+            url: {
+                type: Sequelize.STRING
+            }
+        });
+    }
+};
