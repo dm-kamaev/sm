8c71f2be18761792b1554d81dc060e5ce4be7d28
Dmitry Medvedev <juggernay@yahoo.com>
2016-06-27 21:05:07 +0300

    BP-1262 addressSearchData added

---
+++ api/modules/geo/enums/addressSearchType.js
@@ -0,0 +1,4 @@
+var Enum = require('../../../components/enum');
+module.exports = new Enum('addressSearchType', {
+    EDUCATIONAL_GRADES: 'educational_grades'
+});
---
+++ api/modules/geo/models/addressSearchData.js
@@ -0,0 +1,34 @@
+var Sequelize = require('sequelize');
+
+var sequelize = require('../../../../app/components/db'),
+    addressSearchType = require('../enums/addressSearchType');
+
+var AddressSearchData = db.define('AddressSearchData', {
+    'address_id': {
+        type: Sequelize.INTEGER,
+        onDelete: 'cascade',
+        references: {
+            model: 'address',
+            key: 'id'
+        }
+    },
+    values: Sequelize.ARRAY(Sequelize.INTEGER),
+    type: {
+        type: Sequelize.STRING,
+        validate: {
+            isIn: [addressSearchType.toArray()]
+        }
+    }
+}, {
+    underscored: true,
+    tableName: 'address_search_data',
+    classMethods: {
+        associate: function(models) {
+            AddressSearchData.belongsTo(models.Address, {
+                as: 'address', foreignKey: 'address_id'
+            });
+        }
+    }
+});
+
+module.exports = AddressSearchData;
