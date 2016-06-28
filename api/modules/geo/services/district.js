2daf1b8880011d4736220d4b273cd9a5c4c95f44
Alexander Nedashkovsky <texnik88@gmail.com>
2016-06-28 17:48:46 +0300

    BP-1244: services for district and address

--- api/modules/geo/services/district.js
+++ api/modules/geo/services/district.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const async = require('asyncawait/async');
-const models = require('../../../../app/components/models').all;
+const async = require('asyncawait/async'),
+    await = require('asyncawait/await');
+
+const models = require('../../../../app/components/models').all,
+    geoView = require('../views/geoView');
 
 var service = {
     name: 'district'
@@ -17,6 +20,37 @@ var service = {
  */
 service.create = async(function(districtData) {
     return models.District.create(districtData);
+});
+
+
+/**
+ * Return all districts with areas included
+ * @return {Array<models.District>}
+ */
+service.getAllWithAreas = async(function() {
+   return await(models.District.findAll({
+       include: [{
+           model: models.Area,
+           as: 'areas'
+       }]
+   }));
+});
+
+
+/**
+ * Return coordinates of center of district with given id
+ * @param {number} districtId
+ * @return {Array<number>}
+ */
+service.getCenterCoords = async(function(districtId) {
+    var district = await(models.District.findOne({
+        attributes: ['centerCoords'],
+        where: {
+            id: districtId
+        }
+    }));
+
+    return geoView.coordinatesDefault(district.centerCoords);
 });
 
 module.exports = service;
