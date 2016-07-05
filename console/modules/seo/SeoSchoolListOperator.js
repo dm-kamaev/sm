e9f3d8a135af408c0ed3bbbbfbe7b0c61c152257
Alexander Nedashkovsky <texnik88@gmail.com>
2016-07-05 18:06:43 +0300

    BP-1276: console script to fill seo title and seo description on seo school lists

--- console/modules/seo/SeoSchoolListOperator.js
+++ console/modules/seo/SeoSchoolListOperator.js
@@ -143,7 +143,9 @@ class SeoSchoolListOperator {
      * @param {{
      *     listType: {
      *         name: string,
-     *         value: string
+     *         value: string,
+     *         plural: (string|undefined),
+     *
      *     },
      *     geoType: ({
      *         name: string,
@@ -168,7 +170,10 @@ class SeoSchoolListOperator {
             searchType = itemData.searchType,
             date = new Date().toJSON();
         return {
-            'title': null,
+            'seo_title': this.generateSeoTitle_(listType, geoType, searchType),
+            'seo_description':
+                this.generateSeoDescription_(listType, geoType, searchType),
+            'title': this.generateTitle_(listType, geoType, searchType),
             'text': null,
             'search_parameters': JSON.stringify(
                 this.generateSearchParameters_(listType, geoType, searchType)
@@ -180,13 +185,270 @@ class SeoSchoolListOperator {
         };
     }
 
+    /**
+     * Generate meta title for given list type and geo type
+     * @param {{
+     *     name: string,
+     *     value: string,
+     *     plural: (string|undefined),
+     *     genetive: (string|undefined),
+     *     accusative: (string|undefined)
+     * }} listType
+     * @param {({
+     *     name: string,
+     *     value: string
+     * }|undefined)} geoType
+     * @param {string} searchType
+     * @return {string}
+     */
+    generateSeoDescription_(listType, geoType, searchType) {
+        var result = '';
+        switch (searchType) {
+        case 'institution':
+            result =
+                this.generateInstitutionSeoDescription_(listType);
+            break;
+        case 'district':
+            result = this.generateDistrictSeoDescription_(listType);
+            break;
+        case 'mixed':
+            result =
+                this.generateMixedSeoDescription_(listType, geoType);
+            break;
+        }
+
+        return result;
+    }
+
+    /**
+     * Generate meta title for given institution type
+     * @param {{
+     *     name: string,
+     *     value: string,
+     *     plural: (string|undefined),
+     *     genetive: (string|undefined),
+     *     accusative: (string|undefined)
+     * }} institution
+     * @return {string}
+     * @private
+     */
+    generateInstitutionSeoDescription_(institution) {
+        return 'Список московских ' + institution.genetive +
+            ' с возможностью фильтра и сортировки: по средним оценкам,' +
+            ' по результатам экзаменов и олимпиад, ' +
+            'по отзывам родителей и выпускников. ' +
+            'Лучшие ' + institution.plural +
+            ' города Москвы на одной странице!';
+    }
+
+    /**
+     * Generate meta title for given district name
+     * @param {{
+     *     name: string,
+     *     value: string
+     * }} district
+     * @return {string}
+     * @private
+     */
+    generateDistrictSeoDescription_(district) {
+        return this.generateMixedSeoDescription_(
+            {
+                plural: 'школы',
+                genetive: 'школ'
+            },
+            district
+        );
+    }
+
+    /**
+     * Generate meta title for given institution type and district name
+     * @param {{
+     *     plural: string,
+     *     genetive: string
+     * }} institution
+     * @param {{
+     *     value: string
+     * }} district
+     * @return {string}
+     * @private
+     */
+    generateMixedSeoDescription_(institution, district) {
+        return 'Список ' + institution.genetive + ' ' + district.value +
+            ' Москвы в удобном виде на одной странице. ' +
+            this.capitalizeString_(institution.plural) + ' ' +
+            district.value +
+            ' с возможностью фильтра и сортировки по результатам экзаменов' +
+            ' и олимпиад, отзывам родителей и выпускников.';
+    }
+
+    /**
+     * Generate meta title for given list type and geo type
+     * @param {{
+     *     plural: (string|undefined),
+     *     genetive: (string|undefined),
+     *     accusative: (string|undefined)
+     * }} listType
+     * @param {({
+     *     value: string
+     * }|undefined)} geoType
+     * @param {string} searchType
+     * @return {string}
+     */
+    generateSeoTitle_(listType, geoType, searchType) {
+        var result = '';
+
+        switch (searchType) {
+        case 'institution':
+            result =
+                this.generateInstitutionSeoTitle_(listType);
+            break;
+        case 'district':
+            result = this.generateDistrictSeoTitle_(listType);
+            break;
+        case 'mixed':
+            result =
+                this.generateMixedSeoTitle_(listType, geoType);
+            break;
+        }
+
+        return result;
+    }
+
+    /**
+     * Generate meta title for given institution type
+     * @param {{
+     *     plural: string,
+     *     genetive: string,
+     *     accusative: string
+     * }} institution
+     * @return {string}
+     * @private
+     */
+    generateInstitutionSeoTitle_(institution) {
+        return this.generateInstitutionTitle_(institution.plural) + '. ' +
+            'Выбирайте ' + institution.accusative +
+            ' для поступления! Школы Мела.';
+    }
+
+    /**
+     * Generate meta title for given district name
+     * @param {{
+     *     value: string
+     * }} district
+     * @return {string}
+     * @private
+     */
+    generateDistrictSeoTitle_(district) {
+        return this.generateDistrictTitle_(district.value) + '. Школы Мела.';
+    }
+
+    /**
+     * Generate meta title for given institution type and district name
+     * @param {{
+     *     name: string,
+     *     value: string,
+     *     plural: string
+     * }} institution
+     * @param {{
+     *     name: string,
+     *     value: string
+     * }} district
+     * @return {string}
+     * @private
+     */
+    generateMixedSeoTitle_(institution, district) {
+        return this.generateMixedTitle_(
+                institution.plural, district.value
+            ) + ' . Школы Мела.';
+    }
+
+
+    /**
+     * Generate h1 title for given list type and geo type
+     * @param {{
+     *     name: string,
+     *     value: string,
+     *     plural: (string|undefined),
+     *     genetive: (string|undefined),
+     *     accusative: (string|undefined)
+     * }} listType
+     * @param {({
+     *     name: string,
+     *     value: string
+     * }|undefined)} geoType
+     * @param {string} searchType
+     * @return {string}
+     */
+    generateTitle_(listType, geoType, searchType) {
+        var result = '';
+        switch (searchType) {
+        case 'institution':
+            result =
+                this.generateInstitutionTitle_(listType.plural);
+            break;
+        case 'district':
+            result = this.generateDistrictTitle_(listType.value);
+            break;
+        case 'mixed':
+            result =
+                this.generateMixedTitle_(listType.plural, geoType.value);
+            break;
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Generate h1 title for given institution type
+     * @param {string} institutionType
+     * @return {string}
+     * @private
+     */
+    generateInstitutionTitle_(institutionType) {
+        return this.capitalizeString_(institutionType) + ' Москвы';
+    }
+
+    /**
+     * Generate h1 title for given district name
+     * @param {string} districtName
+     * @return {string}
+     * @private
+     */
+    generateDistrictTitle_(districtName) {
+        return 'Школы ' + districtName + ' Москвы';
+    }
+
+    /**
+     * Generate h1 title for given institution type and district name
+     * @param {string} institutionType
+     * @param {string} districtName
+     * @return {string}
+     * @private
+     */
+    generateMixedTitle_(institutionType, districtName) {
+        return this.capitalizeString_(institutionType) + ' ' +
+            districtName + ' Москвы';
+    }
+
+
+    /**
+     * Capitalizes given string
+     * @param {string} string
+     * @return {string}
+     * @private
+     */
+    capitalizeString_(string) {
+        return ''.concat(string.charAt(0).toUpperCase() + string.slice(1));
+    }
 
     /**
      * Generate search parameters from given listType and geo type.
      * Search for district id and school type id by given values of each type
      * @param {{
      *     name: string,
-     *     value: string
+     *     value: string,
+     *     seoTextForm: (string|undefined)
      * }} listType
      * @param {({
      *     name: string,
@@ -323,9 +585,6 @@ class SeoSchoolListOperator {
             }
         );
     }
-
-
 }
 
 module.exports = SeoSchoolListOperator;
-
