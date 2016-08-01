9121cafb2a90a8fb7d40c7e553a7c479af798293
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-08-01 15:03:04 +0300

    BP-1329: added control for list

--- app/blocks/n-common/b-sm-item-list/b-sm-item-list.js
+++ app/blocks/n-common/b-sm-item-list/b-sm-item-list.js
@@ -1,6 +1,7 @@
 goog.provide('sm.bSmItemList.SmItemList');
 
 goog.require('cl.iControl.Control');
+goog.require('goog.dom.classlist');
 goog.require('sm.bSmItemList.View');
 
 
@@ -14,6 +15,14 @@ goog.require('sm.bSmItemList.View');
  */
 sm.bSmItemList.SmItemList = function(view, opt_domHelper) {
     sm.bSmItemList.SmItemList.base(this, 'constructor', view, opt_domHelper);
+
+
+    /**
+     * Item instances
+     * @type {sm.bSmItem.SmItem|sm.bSmItem.SmItemEntity}
+     * @private
+     */
+    this.items_ = [];
 };
 goog.inherits(sm.bSmItemList.SmItemList, cl.iControl.Control);
 
@@ -21,4 +30,101 @@ goog.inherits(sm.bSmItemList.SmItemList, cl.iControl.Control);
 goog.scope(function() {
     var ItemList = sm.bSmItemList.SmItemList,
         View = sm.bSmItemList.View;
+
+
+    /**
+     * @override
+     */
+    ItemList.prototype.decorateInternal = function(element) {
+        ItemList.base(this, 'decorateInternal', element);
+
+        this.initItems_();
+    };
+
+
+    /**
+     * Add item to top
+     * @param {Object} data
+     */
+    ItemList.prototype.addItemTop = function(data) {
+        this.getView().addItem(data, 0);
+        this.initItems_();
+    };
+
+
+    /**
+     * Add item to bottom
+     * @param {Object} data
+     */
+    ItemList.prototype.addItemBottom = function(data) {
+        var index = this.items_.length;
+
+        this.getView().addItem(data, index);
+        this.initItems_();
+    };
+
+
+    /**
+     * Remove item
+     * @param {sm.bSmItem.SmItem|sm.bSmItem.SmItemEntity} item
+     */
+    ItemList.prototype.removeItem = function(item) {
+        this.removeChild(item);
+
+        var domElement = item.getElement();
+        this.getView().removeItem(domElement);
+
+        this.initItems_();
+    };
+
+
+    /**
+     * Show next hidden items and hide shown items (equal count Items Per Page)
+     */
+    ItemList.prototype.showNextPage = function() {
+        var countPage = this.getView().getCountPage();
+
+        if (this.params.pageNumber < countPage) {
+            this.params.pageNumber++;
+            this.getView().setPage(this.params.pageNumber);
+        }
+    };
+
+
+    /**
+     * Show previous hidden items and hide shown items
+     * (equal count Items Per Page)
+     */
+    ItemList.prototype.showPreviousPage = function() {
+        var countPage = this.getView().getCountPage();
+
+        if (0 <= this.params.pageNumber) {
+            this.params.pageNumber--;
+            this.getView().setPage(this.params.pageNumber);
+        }
+    };
+
+
+    /**
+     * Initializes items
+     * @private
+     */
+    ItemList.prototype.initItems_ = function() {
+        this.getView().initItems();
+
+        var items = this.getView().getDom().items,
+            type = this.params.itemType,
+            instance;
+
+        this.items_ = [];
+
+        for (var i = 0; i < items.length; i++) {
+            instance = this.decorateChild(
+                type,
+                items[i]
+            );
+
+            this.items_.push(instance);
+        }
+    };
 });  // goog.scope
--- app/blocks/n-common/b-sm-item-list/view.js
+++ app/blocks/n-common/b-sm-item-list/view.js
@@ -18,6 +18,17 @@ goog.require('goog.dom.classes');
 sm.bSmItemList.View = function(opt_params, opt_type, opt_modifier) {
     sm.bSmItemList.View.base(this, 'constructor', opt_params,
         opt_type, opt_modifier);
+
+    /**
+     * Parameters
+     * @type {{
+     *     itemType: (smItem,smItemEntity),
+     *     countItemsPerPage: (number|undefined),
+     *     pageNumber: (number|undefined)
+     * }}
+     * @protected
+     */
+    this.params = opt_params || {};
 };
 goog.inherits(sm.bSmItemList.View, cl.iControl.View);
 
@@ -32,5 +43,235 @@ goog.scope(function() {
      */
     View.CssClass = {
         ROOT: 'b-sm-item-list',
+        LIST: 'b-sm-item-list__list',
+        ITEM_CONTAINER: 'b-sm-item-list__item'
+    };
+
+
+    /**
+     * @override
+     * @param {Element} element
+     */
+    View.prototype.decorateInternal = function(element) {
+        View.base(this, 'decorateInternal', element);
+
+        this.initList_(element);
+        this.initParams_();
+    };
+
+
+    /**
+     * Insert item on index (Dom Element)
+     * @param {Object} data
+     * @param {number} index
+     */
+    View.prototype.addItem = function(data, index) {
+        var item = this.renderItem_(data);
+
+        goog.dom.insertChildAt(
+            this.dom.list,
+            item,
+            index
+        );
+    };
+
+
+    /**
+     * Remove item (Dom Element)
+     * @param {Element} item
+     */
+    View.prototype.removeItem = function(item) {
+        var itemContainer = goog.dom.getAncestorByClass(
+            item,
+            sm.bSmItemList.View.CssClass.ITEM_CONTAINER
+        );
+
+        goog.dom.removeNode(
+            itemContainer
+        );
+    };
+
+
+    /**
+     * Set page (show amount items = countItemsPerPage)
+     * @param {number} pageNumber
+     */
+    View.prototype.setPage = function(pageNumber) {
+
+        this.params.pageNumber = pageNumber;
+
+        this.hideItems_(0, this.dom.itemContainers.length);
+
+        var start = pageNumber * this.params.countItemsPerPage,
+            end = start + this.params.countItemsPerPage;
+
+        this.showItems_(start, end);
+    };
+
+
+    /**
+     * Get count page
+     * @return {number}
+     */
+    View.prototype.getCountPage = function() {
+        return Math.ceil(
+            this.dom.itemContainers.length / this.params.countItemsPerPage
+        ) - 1;
+    };
+
+
+    /**
+     * Initializes items (Dom elements)
+     * @param {Element} element
+     */
+    View.prototype.initItems = function(element) {
+        var element = element || this.getElement();
+
+        this.dom.items = this.getElementsByClass(
+            sm.bSmItem.View.CssClass.ROOT,
+            element
+        );
+
+        this.dom.itemContainers = this.getElementsByClass(
+            sm.bSmItemList.View.CssClass.ITEM_CONTAINER,
+            element
+        );
+    };
+
+
+    /**
+     * Show items (from start to end)
+     * @param {number} start
+     * @param {number} end
+     * @private
+     */
+    View.prototype.showItems_ = function(start, end) {
+        for (var i = start; i < end; i++) {
+            itemContainer = this.dom.itemContainers[i];
+
+            if (itemContainer) {
+                this.show_(itemContainer);
+            }
+        }
+    };
+
+
+    /**
+     * Hide items (from start to end)
+     * @param {number} start
+     * @param {number} end
+     * @private
+     */
+    View.prototype.hideItems_ = function(start, end) {
+        for (var i = start; i < end; i++) {
+            itemContainer = this.dom.itemContainers[i];
+
+            if (itemContainer) {
+                this.hide_(itemContainer);
+            }
+        }
+    };
+
+
+    /**
+     * Hide shown Dom Element
+     * @param {Element} element
+     * @private
+     */
+    View.prototype.hide_ = function(element) {
+        var isShown = this.isShown_(element);
+
+        if (isShown) {
+            goog.dom.classlist.add(
+                element,
+                cl.iUtils.Utils.CssClass.HIDDEN
+            );
+        }
+    };
+
+
+    /**
+     * Show hidden Dom Element
+     * @param {Element} element
+     * @private
+     */
+    View.prototype.show_ = function(element) {
+        var isHidden = !this.isShown_(element);
+
+        if (isHidden) {
+            goog.dom.classlist.remove(
+                element,
+                cl.iUtils.Utils.CssClass.HIDDEN
+            );
+        }
+    };
+
+
+    /**
+     * Check contains an element of class HIDDEN or not
+     * @param {Element} element
+     * @return {bool}
+     * @private
+     */
+    View.prototype.isShown_ = function(element) {
+        return !goog.dom.classlist.contains(
+            element,
+            cl.iUtils.Utils.CssClass.HIDDEN
+        );
+    };
+
+
+    /**
+     * Render Item
+     * @param {Object} data
+     * @return {Element}
+     * @private
+     */
+    View.prototype.renderItem_ = function(data) {
+        return goog.soy.renderAsElement(
+            sm.bSmItemList.Template.item, {
+                params: {
+                    'data': {
+                        'item': data
+                    },
+                    'config': {
+                        'type': this.params.itemType
+                    }
+                }
+            }
+        );
+    };
+
+
+    /**
+     * Initializes list (container for items)
+     * @param {Element} element
+     * @private
+     */
+    View.prototype.initList_ = function(element) {
+        var element = element || this.getElement();
+
+        this.dom.list = this.getElementByClass(
+            sm.bSmItemList.View.CssClass.LIST,
+            element
+        );
+    };
+
+
+    /**
+     * Initializes params from attributes data-params
+     * @private
+     */
+    View.prototype.initParams_ = function() {
+        this.params = JSON.parse(
+            goog.dom.dataset.get(
+                this.getElement(),
+                'params'
+            )
+        );
+
+        if (this.params.countItemsPerPage) {
+            this.params.pageNumber = 0;
+        }
     };
 });  // goog.scope
