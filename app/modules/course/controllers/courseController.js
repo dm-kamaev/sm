b2bb0fa1de36376f33d2eaef613bd58d043e62dd
Alexander Nedashkovsky <texnik88@gmail.com>
2016-08-04 19:17:12 +0300

    BP-1342: search course controller init

---
+++ app/modules/course/controllers/courseController.js
@@ -0,0 +1,95 @@
+const services = require('../../../components/services').all;
+
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
+const soy = require('../../../components/soy');
+
+const config = require('../../../config').config;
+const ANALYTICS_ID = config.analyticsId;
+const YANDEX_METRIKA_ID = config.yandexMetrikaId;
+const DOMAIN = config.url.protocol + '://' + config.url.host;
+const FB_CLIENT_ID = config.facebookClientId;
+
+exports.search = async(function(req, res, next) {
+    try {
+        var searchParams =
+            await(services.search.initSearchParams(req.query));
+        var authSocialLinks = await(services.auth.getAuthSocialUrl());
+
+        var params = {
+            data: {
+                seo: {
+                    metaTitle: 'Кружки и секции'
+                },
+                subHeader: {
+                    logo: {
+                        imgUrl: '/images/n-common/b-sm-subheader/course-logo.svg'
+                    },
+                    links: {
+                        nameL: 'Все курсы, кружки и секции',
+                        nameM: 'Все курсы',
+                        url: '/'
+                    },
+                    search: {},
+                    user: null,
+                    favorites: []
+                },
+                user: null,
+                authSocialLinks: authSocialLinks,
+                map: {
+                    schools: []
+                },
+                search: {
+                    countResults: 100,
+                    searchText: 'Супер курс'
+                },
+                sort: {
+                    listItems: [
+                        {
+                            'label': 'Средняя оценка',
+                            'text': 'средней оценке'
+                        },
+                        {
+                            'label': 'Качество',
+                            'text': 'качеству'
+                        }
+                    ],
+                    staticText: 'Сортировать по ',
+                    defaultOpenerText: 'стоимости'
+                },
+                entityList: [],
+                searchSettings: {
+                    url: 'api/course/search',
+                    method: 'GET',
+                    searchParams: searchParams
+                }
+            },
+            config: {
+                landingScript: 'l-sm-search.js',
+                modifier: 'stendhal',
+                staticVersion: config.lastBuildTimestamp,
+                year: new Date().getFullYear(),
+                analyticsId: ANALYTICS_ID,
+                yandexMetrikaId: YANDEX_METRIKA_ID,
+                csrf: req.csrfToken(),
+                domain: DOMAIN,
+                fbClientId: FB_CLIENT_ID
+            }
+        };
+        var html = soy.render(
+            'sm.lSmSearch.Template.search',
+            {
+                params: params
+            }
+        );
+
+        res.header('Content-Type', 'text/html; charset=utf-8');
+        res.end(html);
+    } catch (error) {
+        console.log(error);
+
+        res.status(error.code || 500);
+        next();
+    }
+});
