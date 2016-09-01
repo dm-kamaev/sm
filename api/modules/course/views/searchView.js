de4d8dc889f9640255e7b3a44ae21bf479927932
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-09-01 18:04:54 +0300

    BP-1345: fix searchView for course

--- api/modules/course/views/searchView.js
+++ api/modules/course/views/searchView.js
@@ -1,16 +1,20 @@
+var FilterPanel = require('../lib/CourseFilterPanel');
+
+var searchView = {};
+
 /**
  * Data for filter panel
  * @param {Array<Object>} filtersData
  * @param {{
- *     age: Array<number>,
- *     course: Array<string>,
- *     cost: Array<string>,
- *     schedule: Array<string>,
- *     time: Array<string>,
- *     regularity: Array<string>,
- *     formTraining: Array<string>,
- *     duration: Array<string>
- * }} searchParams
+ *     age: Array<(string|number)>,
+ *     course: Array<(string|number)>,
+ *     cost: Array<(string|number)>,
+ *     schedule: Array<(string|number)>,
+ *     time: Array<(string|number)>,
+ *     regularity: Array<(string|number)>,
+ *     formTraining: Array<(string|number)>,
+ *     duration: Array<(string|number)>
+ * }} opt_searchParams
  * @return {{
  *     data: {
  *         filters: Array<{
@@ -47,594 +51,45 @@
  *     }
  * }}
  */
-module.exports.filterPanel = function(filtersData, searchParams) {
-    var filters = this.filters(filtersData, searchParams);
+searchView.filterPanel = function(filtersData, opt_searchParams) {
+    var searchParams = opt_searchParams || {};
 
-    return {
-        data: {
-            filters: filters
-        },
-        config: {
-            hasCheckedFilters: ''
-        }
-    };
-};
+    var options = [{
+        'label': 'Подготовка к ЕГЭ по русскому языку',
+        'value': '1'
+    }, {
+        'label': 'Подготовка к ЕГЭ по математике',
+        'value': '2'
+    }, {
+        'label': 'Подготовка к ОГЭ по русскому языку',
+        'value': '3'
+    }, {
+        'label': 'Подготовка к ОГЭ по математике',
+        'value': '4'
+    }, {
+        'label': 'Игра на фортепиано',
+        'value': '5'
+    }, {
+        'label': 'Русский язык',
+        'value': '6'
+    }, {
+        'label': 'Математика',
+        'value': '7'
+    }];
 
+    var filterPanel = new FilterPanel();
 
-/**
- * Generate filters (filters and selected options filters) for course
- * @param {Array<Object>} filtersData
- * @param {{
- *     age: Array<number>,
- *     course: Array<string>,
- *     cost: Array<string>,
- *     schedule: Array<string>,
- *     time: Array<string>,
- *     regularity: Array<string>,
- *     formTraining: Array<string>,
- *     duration: Array<string>
- * }} searchParams
- * @return {
- *     Array<{
- *         data: {
- *             header: {
- *                 title: (string|undefined),
- *                 tooltip: (string|undefined)
- *             },
- *             name: string,
- *             options: Array<({
- *                 label: string,
- *                 value: string,
- *                 isChecked: boolean
- *               }|{
- *                 title: (string|undefined),
- *                 value: (number|undefined),
- *                 placeholder: (string|undefined),
- *                 maxLength: (number|undefined)
- *             })>
- *         },
- *         config: {
- *             type: (string|undefined),
- *             optionsToShow: (boolean|undefined),
- *             cannotBeHidden: (boolean|undefined),
- *             isShowed: (boolean|undefined),
- *             showMoreButtonText: (number|undefined),
- *             theme: (string|undefined),
- *             align: (string|undefined)
- *         }
- *     }>
- * }
- */
-module.exports.filters = function(filtersData, searchParams) {
-    /** here makes order of filters in left menu **/
-    var filters = [];
+    filterPanel
+        .setFilterAge(searchParams.age)
+        .setFilterCourse(options, searchParams.course)
+        .setFilterCost(searchParams.cost)
+        .setFilterWeekDays(searchParams.schedule)
+        .setFilterTime(searchParams.time)
+        .setFilterRegularity(searchParams.regularity)
+        .setFilterFormTraining(searchParams.formTraining)
+        .setFilterDuration(searchParams.duration);
 
-    filters.push(
-        this.filterAge(),
-        this.filterCourse(),
-        this.filterCost(),
-        this.filterSchedule(),
-        this.filterTime(),
-        this.filterRegularity(),
-        this.filterFormTraining(),
-        this.filterDuration()
-    );
-
-    return filters;
-};
-
-
-/**
- * Filter for selection courses by age
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             title: (string|undefined),
- *             value: (number|undefined),
- *             placeholder: (string|undefined),
- *             maxLength: (number|undefined)
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterAge = function() {
-    return {
-        data: {
-            header: {
-                title: 'Возраст'
-            },
-            name: 'age',
-            options: [
-                {
-                    'title': 'лет',
-                    'value': '',
-                    'maxLength': 2
-                }
-            ]
-        },
-        config: {
-            type: 'input'
-        }
-    };
-};
-
-
-/**
- * Filter to select courses
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterCourse = function() {
-    var options = [
-        {
-            'label': 'Подготовка к ЕГЭ по русскому языку',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Подготовка к ЕГЭ по математике',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Подготовка к ОГЭ по русскому языку',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Подготовка к ОГЭ по математике',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Игра на фортепиано',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Русский язык',
-            'value': '',
-            'isChecked': false
-        },
-        {
-            'label': 'Математика',
-            'value': '',
-            'isChecked': false
-        }
-    ];
-
-    return {
-        data: {
-            header: {
-                title: 'Направления занятий'
-            },
-            name: 'course',
-            options: options
-        },
-        config: {
-            type: 'extended',
-            showMoreButtonText: 'Все направления'
-        }
-    };
-};
-
-
-/**
- * Filter to select courses cost
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterCost = function() {
-    return {
-        data: {
-            header: {
-                title: 'Стоимость'
-            },
-            name: 'cost',
-            options: [
-                {
-                    'label': 'Бесплатно',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'До 1 000 руб',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'От 1 000 до 2 000 руб',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'От 2 000 руб',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Скрыть, если не указана цена',
-                    'value': '',
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-        }
-    };
-};
-
-
-/**
- * Filter to select courses schedule
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterSchedule = function() {
-    return {
-        data: {
-            header: {
-                title: 'Расписание'
-            },
-            name: 'schedule',
-            options: [
-                {
-                    'label': 'Пн',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Вт',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Ср',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Чт',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Пт',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Сб',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Вс',
-                    'value': '',
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-            align: 'horizontal',
-            optionsToShow: 7
-        }
-    };
-};
-
-
-/**
- * Filter to select courses time
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterTime = function() {
-    return {
-        data: {
-            header: {
-                title: 'Время занятий'
-            },
-            name: 'time',
-            options: [
-                {
-                    'label': 'Утром (до 14:00)',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Днём (14:00 - 18:00)',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Вечером (18:00 и позже)',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Скрыть, если не указано расписание',
-                    'value': '',
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-        }
-    };
-};
-
-
-/**
- * Filter to select the regularity of training courses
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterRegularity = function() {
-    return {
-        data: {
-            header: {
-                title: 'Регулярность занятий'
-            },
-            name: 'regularity',
-            options: [
-                {
-                    'label': 'Раз в неделю',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Два раза в неделю',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Чаще двух раз в неделю',
-                    'value': '',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Скрыть, если не указана регулярность',
-                    'value': '',
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-        }
-    };
-};
-
-
-/**
- * Filter to select the form of training courses
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterFormTraining = function() {
-    return {
-        data: {
-            header: {
-                title: 'Форма занятий'
-            },
-            name: 'formTraining',
-            options: [
-                {
-                    'label': 'Очная индивидуальная',
-                    'value': 'individual',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Очная групповая',
-                    'value': 'group',
-                    'isChecked': false
-                },
-                {
-                    'label': 'Онлайн',
-                    'value': 'online',
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-        }
-    };
-};
-
-
-/**
- * Filter to select the duration of lesson
- * @return {{
- *     data: {
- *         header: {
- *             title: (string|undefined),
- *             tooltip: (string|undefined)
- *         },
- *         name: string,
- *         options: Array<{
- *             label: string,
- *             value: string,
- *             isChecked: boolean
- *         }>
- *     },
- *     config: {
- *         type: (string|undefined),
- *         optionsToShow: (boolean|undefined),
- *         cannotBeHidden: (boolean|undefined),
- *         isShowed: (boolean|undefined),
- *         showMoreButtonText: (number|undefined),
- *         theme: (string|undefined),
- *         align: (string|undefined)
- *     }
- * }}
- */
-module.exports.filterDuration = function() {
-    return {
-        data: {
-            header: {
-                title: 'Продолжительность занятий'
-            },
-            name: 'duration',
-            options: [
-                {
-                    'label': 'До 1 часа',
-                    'value': 0,
-                    'isChecked': false
-                },
-                {
-                    'label': '1-2 часа',
-                    'value': 1,
-                    'isChecked': false
-                },
-                {
-                    'label': 'более двух часов',
-                    'value': 2,
-                    'isChecked': false
-                }
-            ]
-        },
-        config: {
-        }
-    };
+    return filterPanel.getParams();
 };
 
 /**
@@ -648,7 +103,7 @@ module.exports.filterDuration = function() {
  * }} data
  * @return {Object}
  */
-module.exports.render = function(data) {
+searchView.render = function(data) {
     return {
         seo: {
             metaTitle: 'Кружки и секции'
@@ -705,7 +160,9 @@ module.exports.render = function(data) {
             items: data.coursesList,
             itemType: 'smItemEntity'
         },
-        filterPanel: this.filterPanel(),
+        filterPanel: searchView.filterPanel(null, data.searchParams),
         searchParams: data.searchParams
     };
 };
+
+module.exports = searchView;
