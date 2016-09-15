36c2e6a0a61417a431fee6c69a95b00c43bc299f
Dmitry Medvedev <juggernay@yahoo.com>
2016-09-15 16:18:31 +0300

    BP-1457 backend linter fix

--- api/modules/course/migrations/20160915155900-alter-course-brand-name.js
+++ api/modules/course/migrations/20160915155900-alter-course-brand-name.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const async = require('asyncawait/async'),
-    await = require('asyncawait/await');
+const async = require('asyncawait/async');
 
 module.exports = {
     up: async(function(queryInterface, Sequelize) {
@@ -11,7 +10,7 @@ module.exports = {
             unique: true
         });
     }),
-    down: async(function(queryInterface) {
+    down: async(function(queryInterface, Sequelize) {
         return queryInterface.changeColumn('course_brand', 'name', {
             type: Sequelize.STRING,
             allowNull: false
