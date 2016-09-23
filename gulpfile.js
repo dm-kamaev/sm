7fadf47e31c1ff099e15e484772ec3c0e794f6a4
Dmitry Medvedev <juggernay@yahoo.com>
2016-09-23 11:48:22 +0300

    BP-1483 app and root folders added to linter check

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const concat = require('gulp-concat');
 const util = require('gulp-util');
 const babel = require('gulp-babel');
 const apidoc = require('gulp-apidoc');
-const fs = require('fs-extra');
 const exec = require('child_process').exec;
 const eslint = require('gulp-eslint');
 
@@ -26,10 +25,10 @@ const ENV = util.env.env ? util.env.env : 'dev';
 
 const gulpTasks = require('./gulp')(gulpHelper);
 
-gulp.task('doc', function () {
+gulp.task('doc', function() {
     apidoc({
-        src: "./api/modules/",
-        dest: "./doc"
+        src: './api/modules/',
+        dest: './doc'
     }, function() {
     });
 });
@@ -42,25 +41,25 @@ gulp.task('lint', function() {
     });
 });
 
-gulp.task('migrate', function () {
+gulp.task('migrate', function() {
     return gulpHelper.migrate.build({
         src: path.join(__dirname, 'api/**/migrations/*.js')
     });
 });
 
 
-gulp.task('appES5', function () {
+gulp.task('appES5', function() {
     return gulp.src('app.js')
         .pipe(babel())
         .pipe(concat('appES5.js'))
         .pipe(gulp.dest(''));
 });
 
-gulp.task('soy', function () {
+gulp.task('soy', function() {
     return gulpHelper.soy.build();
 });
 
-gulp.task('styles', ['sprite'], function () {
+gulp.task('styles', ['sprite'], function() {
     return gulpHelper.css.build({
         outputFiles: [{
             src: [
@@ -76,14 +75,22 @@ gulp.task('styles', ['sprite'], function () {
 
 gulp.task('sprite', function() {
     return gulpHelper.sprite.build([{
-        src: path.join(__dirname, BLOCKS_DIR, '/n-clobl/g-icon/g-icon_img/*.png'),
+        src: path.join(
+            __dirname,
+            BLOCKS_DIR,
+            '/n-clobl/g-icon/g-icon_img/*.png'
+        ),
         retina: {
-            filters: [path.join(__dirname, BLOCKS_DIR, '/n-clobl/g-icon/g-icon_img/*@2x.png')]
+            filters: [path.join(
+                __dirname,
+                BLOCKS_DIR,
+                '/n-clobl/g-icon/g-icon_img/*@2x.png'
+            )]
         }
     }]);
 });
 
-gulp.task('images', function () {
+gulp.task('images', function() {
     var src = ['png', 'ico', 'svg', 'gif', 'jpg']
             .map(ext => '**/*.' + ext)
             .map(mask => path.join(__dirname, BLOCKS_DIR, mask)),
@@ -93,34 +100,33 @@ gulp.task('images', function () {
         .pipe(gulp.dest(dest));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch(
-        [path.join(__dirname + BLOCKS_DIR + '/**/*.soy')],
+        [path.join(__dirname, BLOCKS_DIR, '/**/*.soy')],
         ['scripts']
     );
     gulp.watch(
-        [path.join(__dirname + BLOCKS_DIR + '/**/*.js')],
+        [path.join(__dirname, BLOCKS_DIR, '/**/*.js')],
         ['scripts']
     );
     gulp.watch([
-        path.join(__dirname + BLOCKS_DIR + '/**/*.scss'),
-        path.join(__dirname + BLOCKS_DIR + '/**/*.css')
+        path.join(__dirname, BLOCKS_DIR, '/**/*.scss'),
+        path.join(__dirname, BLOCKS_DIR, '/**/*.css')
     ], ['styles']);
 });
 
-gulp.task('fonts', function () {
-    return gulp.src(path.join(__dirname + '/assets/fonts/**/*.*'))
-        .pipe(gulp.dest(path.join(__dirname + '/public/fonts')));
+gulp.task('fonts', function() {
+    return gulp.src(path.join(__dirname, '/assets/fonts/**/*.*'))
+        .pipe(gulp.dest(path.join(__dirname, '/public/fonts')));
 });
 
 gulp.task('copy', function() {
     return gulp.src([
-            path.join(__dirname + '/assets/robots.txt'),
-            path.join(__dirname + '/assets/google86acdf989d7328cf.html'),
-            path.join(__dirname + '/assets/yandex_7cfaf013e2f3373d.html')
-        ],
-        {base: 'assets/'})
-        .pipe(gulp.dest('public'));
+        path.join(__dirname, '/assets/robots.txt'),
+        path.join(__dirname, '/assets/google86acdf989d7328cf.html'),
+        path.join(__dirname, '/assets/yandex_7cfaf013e2f3373d.html')
+    ], {base: 'assets/'})
+         .pipe(gulp.dest('public'));
 });
 
 gulp.task('localConfig', function() {
@@ -133,25 +139,28 @@ gulp.task('localConfig', function() {
     });
 });
 
-gulp.task('backendLint', function () {
-    return gulp.src('api/**/*.js')
+gulp.task('backendLint', function() {
+    return gulp.src([
+        'api/**/*.js',
+        'app/modules/!(doc)**/*.js',
+        './*.js'])
         .pipe(eslint({
             config: path.join(__dirname, 'node_modules/nodules/.eslintrc')
         }))
         .pipe(eslint.format())
-        .pipe(eslint.results((result => {
+        .pipe(eslint.results(result => {
             if (result.errorCount) {
                 throw new Error('Error count: ' + result.errorCount +
                     '. Linter check failed!');
             }
-        })));
+        }));
 });
 
-const tasks = function (bool) {
+const tasks = function(bool) {
     return bool ?
         ['createTimestamp', 'soy', 'compile', 'sprite', 'images', 'fonts',
             'styles', 'copy', 'localConfig'] :
-        ['watch', 'soy', 'scripts', 'sprite', 'images', 'fonts','styles',
+        ['watch', 'soy', 'scripts', 'sprite', 'images', 'fonts', 'styles',
             'copy', 'localConfig', 'backendLint'];
 };
 
