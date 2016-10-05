20ce1a868248ede0de7954ec84b3f57a9684dfd0
Misha Pachin <misha.pachin@gmail.com>
2016-10-05 17:10:18 +0300

    BP-1448 fix google styleguide

--- app/middleware/usercheck.js
+++
@@ -1,9 +0,0 @@
-var pass = require('../base/config').config["admin-token"];
-module.exports = function(req, res, next) {
-    if(req.headers["admin-token"] === pass) {
-        next();
-    } else {
-        res.status(403);
-        res.end();
-    }
-};
