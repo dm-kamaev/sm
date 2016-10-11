b92c74efe28e7bc9a0746101d042dd13c6594d56
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-10-11 16:46:15 +0300

    BP-1484: fix button loader in l-search

--- app/blocks/n-common/l-search/l-search.js
+++ app/blocks/n-common/l-search/l-search.js
@@ -175,7 +175,8 @@ goog.scope(function() {
             .initSearchServiceListeners_()
             .initResultsListListeners_()
             .initWindowListeners_()
-            .initMapListeners_();
+            .initMapListeners_()
+            .initLoadMoreResultsListItemsListeners_();
 
         this.sendAnalyticsItemsLoad_(1);
     };
@@ -247,11 +248,7 @@ goog.scope(function() {
             this.onListItemClick_
         );
 
-        var loadedItemsAmount = this.resultsList_.getCountItems();
-        this.detectShowMoreResultsList_(
-            loadedItemsAmount, this.params.countResults
-        );
-
+        this.detectShowMoreResultsList_();
         return this;
     };
 
@@ -416,11 +413,13 @@ goog.scope(function() {
      */
     Search.prototype.onResultsListDataLoaded_ = function(event) {
         var listItems = event.getListItems();
-        var countResults = event.getCountResults();
-        this.updateResultsList_(listItems, countResults);
-        this.detectShowMoreResultsList_(listItems.length, countResults);
+        this.params.countResults = event.getCountResults();
 
-        this.getView().setSortVisibility(this.resultsList_.getCountItems());
+        this.updateResultsList_(listItems, this.params.countResults);
+        this.detectShowMoreResultsList_();
+
+        var sortVisibility = this.resultsList_.getCountItems() ? true : false;
+        this.getView().setSortVisibility(sortVisibility);
         this.getView().setLoaderVisibility(false);
 
         this.sendAnalyticsItemsLoad_(0);
@@ -558,17 +557,12 @@ goog.scope(function() {
      * Detect whether results list can be loaded more items.
      * If all items loaded, then results list cannot add more items and send
      * queries to backend to load more results
-     * @param  {number} countListItems amount of currently loaded items
-     * @param  {number} countResults overall amount of search results
      * @private
      */
-    Search.prototype.detectShowMoreResultsList_ = function(
-        countListItems, countResults) {
-        if (this.isAllSearchItemsLoaded_(countListItems, countResults)) {
-            this.disableLoadMoreResultsListItems_();
+    Search.prototype.detectShowMoreResultsList_ = function() {
+        if (this.isAllSearchItemsLoaded_()) {
             this.getView().setShowMoreButtonVisibility(false);
         } else {
-            this.enableLoadMoreResultsListItems_();
             this.getView().setShowMoreButtonVisibility(true);
         }
     };
@@ -576,44 +570,20 @@ goog.scope(function() {
 
     /**
      * Check if all items of current search parameters loaded
-     * @param {number} loadedItemsAmount
-     * @param {number} countResults
      * @return {boolean}
      * @private
      */
-    Search.prototype.isAllSearchItemsLoaded_ = function(
-        loadedItemsAmount,
-        countResults) {
-        var listItemsAmount = this.resultsList_.getCountItems();
-        return (loadedItemsAmount < Search.SEARCH_CHUNK_SIZE) ||
-            (countResults == listItemsAmount);
+    Search.prototype.isAllSearchItemsLoaded_ = function() {
+        return this.resultsList_.getCountItems() == this.params.countResults;
     };
 
 
     /**
-     * Disable loading more items to results list
-     * Unlisten scroll and show more button click events
-     * to disable load more items to results list
+     * Init listeners event for loading new data
+     * @return {sm.lSearch.Search}
      * @private
      */
-    Search.prototype.disableLoadMoreResultsListItems_ = function() {
-        this.getHandler().unlisten(
-            this.showMoreButton_,
-            cl.gButton.Button.Event.CLICK
-        ).unlisten(
-            goog.dom.getWindow(),
-            goog.events.EventType.SCROLL
-        );
-    };
-
-
-    /**
-     * Enable loading more items to results list
-     * Listen scroll and show more button click events
-     * to enable load more items to results list
-     * @private
-     */
-    Search.prototype.enableLoadMoreResultsListItems_ = function() {
+    Search.prototype.initLoadMoreResultsListItemsListeners_ = function() {
         this.getHandler().listen(
             this.showMoreButton_,
             cl.gButton.Button.Event.CLICK,
@@ -623,6 +593,7 @@ goog.scope(function() {
             goog.events.EventType.SCROLL,
             this.onScroll_
         );
+        return this;
     };
 
 
@@ -644,12 +615,16 @@ goog.scope(function() {
      * @private
      */
     Search.prototype.loadNextPage_ = function() {
-        this.paramsManager_.increasePage();
+        if (!this.isAllSearchItemsLoaded_()) {
+            this.paramsManager_.increasePage();
 
-        this.getView().setLoaderVisibility(true);
-        this.getView().setShowMoreButtonVisibility(false);
+            this.getView().setLoaderVisibility(true);
+            this.getView().setShowMoreButtonVisibility(false);
 
-        this.searchService_.loadSearchData(this.paramsManager_.getParams());
+            this.searchService_.loadSearchData(
+                this.paramsManager_.getParams()
+            );
+        }
     };
 
 
