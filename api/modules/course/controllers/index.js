2f292f6fd2aae481c0a70d0aa8fe17b74c265549
Dmitry Medvedev <juggernay@yahoo.com>
2016-10-20 22:47:06 +0300

    BP-1589 routing refactoring

--- api/modules/course/controllers/index.js
+++ api/modules/course/controllers/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+let express = require('express');
+let router = express.Router();
 
 const courseController = require('./courseController'),
     brandController = require('./brandController'),
@@ -10,8 +12,6 @@ const courseController = require('./courseController'),
     csrf = require('../../../../app/middleware/csrf'),
     checkToken = require('../../../../app/middleware/checkToken');
 
-const DEPARTMENT_ROUTE = '/coursebrand/:brandId/department';
-
 router.get('/course/search', courseController.search);
 router.get('/course/search/map', courseController.searchMap);
 router.get('/course/search/suggest', courseController.suggestSearch);
@@ -21,48 +21,18 @@ router.get('/course/course-type', courseController.searchCourseType);
 
 router.post('/course/enrollment', csrf, courseController.enrollOnCourse);
 
-router.get('/coursebrand', brandController.list);
-router.get('/coursebrand/:id', brandController.get);
-router.post('/coursebrand', checkToken, brandController.create);
-router.put('/coursebrand/:id', checkToken, brandController.update);
-router.delete('/coursebrand/:id', checkToken, brandController.delete);
+let initCrudRouting = function(route, controller) {
+    router.post(`${route}`, checkToken, controller.create);
+    router.get(`${route}`, controller.list);
+    router.get(`${route}/:id`, controller.get);
+    router.put(`${route}/:id`, checkToken, controller.update);
+    router.delete(`${route}/:id`, checkToken, controller.delete);
+};
 
-router.get('/course', courseController.list);
-router.get('/course/:id', courseController.get);
-router.post('/course', checkToken, courseController.create);
-router.put('/course/:id', checkToken, courseController.update);
-router.delete('/course/:id', checkToken, courseController.delete);
-
-router.get(DEPARTMENT_ROUTE, departmentController.list);
-router.get(`${DEPARTMENT_ROUTE}/:id`, departmentController.get);
-router.post(DEPARTMENT_ROUTE, checkToken, departmentController.create);
-router.put(`${DEPARTMENT_ROUTE}/:id`, checkToken, departmentController.update);
-router.delete(
-    `${DEPARTMENT_ROUTE}/:id`,
-    checkToken,
-    departmentController.delete
-);
-
-router.get('/course/:courseId/option', optionController.list);
-router.get('/course/:courseId/option/:id', optionController.get);
-router.post('/course/:courseId/option', checkToken, optionController.create);
-router.put('/course/:courseId/option/:id', checkToken, optionController.update);
-router.delete(
-    '/course/:courseId/option/:id',
-    checkToken,
-    optionController.delete
-);
-
-router.get('/coursecategory', categoryController.list);
-router.get('/coursecategory/:id', categoryController.get);
-router.post('/coursecategory', checkToken, categoryController.create);
-router.put('/coursecategory/:id', checkToken, categoryController.update);
-router.delete('/coursecategory/:id', checkToken, categoryController.delete);
-
-router.get('/coursetype', typeController.list);
-router.get('/coursetype/:id', typeController.get);
-router.post('/coursetype', checkToken, typeController.create);
-router.put('/coursetype/:id', checkToken, typeController.update);
-router.delete('/coursetype/:id', checkToken, typeController.delete);
+initCrudRouting('/course', courseController);
+initCrudRouting('/coursebrand/:brandId/department', departmentController);
+initCrudRouting('/course/:courseId/option', optionController);
+initCrudRouting('/coursecategory', categoryController);
+initCrudRouting('/coursetype', typeController);
 
 module.exports = router;
