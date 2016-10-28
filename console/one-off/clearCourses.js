26a7ef85fb39fd6a3ef2d053afaf6fe4b25c9be3
Dmitry Medvedev <juggernay@yahoo.com>
2016-10-28 15:33:07 +0300

    BP-1641 clear course scirpt fix

--- console/one-off/clearCourses.js
+++ console/one-off/clearCourses.js
@@ -9,7 +9,7 @@ const DELETE_QUERIES = [
     'DELETE FROM course_category',
     'DELETE FROM course_type',
     'DELETE FROM address WHERE entity_type != \'school\'',
-    'DELETE FROM page WHERE entity_type !+ \'school\''
+    'DELETE FROM page WHERE entity_type != \'school\''
 ];
 
 module.exports = async(function() {
