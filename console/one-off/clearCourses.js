e8ce0fd38c8a6c2cfde0a2844b84ebb2c6f78c04
Dmitry Medvedev <juggernay@yahoo.com>
2016-10-31 21:42:19 +0300

    BP-1641 db sequence restore

--- console/one-off/clearCourses.js
+++ console/one-off/clearCourses.js
@@ -2,15 +2,24 @@
 
 const async = require('asyncawait/async'),
     await = require('asyncawait/await'),
-    sequelize = require('../../app/components/db');
+    sequelize = require('../../app/components/db'),
+    SqlHelper = require('../modules/sqlHelper/SqlHelper');
 
-const DELETE_QUERIES = [
-    'DELETE FROM course_brand',
-    'DELETE FROM course_category',
-    'DELETE FROM course_type',
-    'DELETE FROM address WHERE entity_type != \'school\'',
-    'DELETE FROM page WHERE entity_type != \'school\''
-];
+const ACTUALIZE_TABLES = [
+        'course_brand',
+        'course_category',
+        'course_type',
+        'address',
+        'page',
+        'course'
+    ],
+    DELETE_QUERIES = [
+        `DELETE FROM ${ACTUALIZE_TABLES[0]}`,
+        `DELETE FROM ${ACTUALIZE_TABLES[1]}`,
+        `DELETE FROM ${ACTUALIZE_TABLES[2]}`,
+        `DELETE FROM ${ACTUALIZE_TABLES[3]} WHERE entity_type != 'school'`,
+        `DELETE FROM ${ACTUALIZE_TABLES[4]} WHERE entity_type != 'school'`
+    ];
 
 module.exports = async(function() {
     await(DELETE_QUERIES.map(query =>
@@ -19,6 +28,9 @@ module.exports = async(function() {
                 type: sequelize.QueryTypes.DELETE
             }
         )
+    ));
+    await(ACTUALIZE_TABLES.map(tableName =>
+        SqlHelper.actualizeSequence(tableName)
     ));
 });
 
