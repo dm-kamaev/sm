a323ef5fbdff7ceaa38eb8d67ae018c19c8b9bb3
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-11-09 12:59:08 +0300

    BP-1690: added new gulp task for svg sprites

---
+++ gulp/taskSvgSprite.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const gulp = require('gulp');
+const path = require('path');
+
+const svgSprite = require('gulp-svg-sprites');
+const gulpFilter = require('gulp-filter');
+
+const Path = require('./Path');
+
+module.exports = function() {
+    let streamSprite = gulp
+        .src(path.join(
+            __dirname,
+            Path.BLOCKS_DIR,
+            '/n-clobl/g-icon/g-icon_img/*.svg'
+        ))
+        .pipe(svgSprite({
+            preview: false,
+            selector: '%f',
+            common: 'g-icon',
+            cssFile: 'svg-sprite.scss',
+            svgPath: '%f',
+            svg: {
+                sprite: 'images/g-icon_auto-svg-sprite.svg'
+            }
+        }))
+        .pipe(gulp.dest(path.join(__dirname)));
+
+    streamSprite
+        .pipe(gulpFilter('**/*.svg'))
+        .pipe(gulp.dest(
+            path.join(__dirname, '../', Path.SHARED_STATIC_DIR)
+        ));
+
+    streamSprite
+        .pipe(gulpFilter('**/*.scss'))
+        .pipe(gulp.dest(path.join(__dirname, '../build')));
+};
