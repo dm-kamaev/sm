0181a5d2d805c7938d263195772809a68879361b
Alexander Nedashkovsky <texnik88@gmail.com>
2016-11-14 12:35:24 +0300

    BP-1693: analytics report loader

--- console/modules/googleAnalytics/AnalyticsReportLoader.js
+++ console/modules/googleAnalytics/AnalyticsReportLoader.js
@@ -1,9 +1,57 @@
+/**
+ * @fileoverview Class for load data from goople analytics api and update it
+ * on courses
+ */
 'use strict';
+
+const await = require('asyncawait/await');
+const lodash = require('lodash');
+
+const services = require('../../../app/components/services').all;
+const sequelize = require('../../../app/components/db');
+const logger = require('../../../app/components/logger/logger')
+    .getLogger('app');
 
 const googleApi = require('googleapis');
 const key = require('../../../assets/school-market-55aa2d283f38.json');
 
 const VIEW_ID = 'ga:130630878';
+
+/**
+ * Api parameters
+ */
+const VERSION = 'v3';
+
+/**
+ * api request parameters
+ */
+const LIST_NAME_DIMENTION = 'ga:productListName',
+    LIST_NAME_VALUE = 'search results',
+    START_DATE = '14daysAgo',
+    END_DATE = 'today';
+const PARAMS = {
+    productName: {
+        queryName: 'ga:productName',
+        name: 'name'
+    },
+    category: {
+        queryName: 'ga:productCategoryHierarchy',
+        name: 'category'
+    },
+    ctr: {
+        queryName: 'ga:productListCTR',
+        name: 'ctr'
+    },
+    clicks: {
+        queryName: 'ga:productListClicks',
+        name: 'clicks'
+    },
+    views: {
+        queryName: 'ga:productListViews',
+        name: 'views'
+    }
+};
+const MAX_RESULTS = 500;
 
 class AnalyticsReportLoader {
 
@@ -15,7 +63,7 @@ class AnalyticsReportLoader {
          * Google analytics api instance
          * @type {Object}
          */
-        this.analyticsApi_ = googleApi.analytics('v3');
+        this.analyticsApi_ = googleApi.analytics(VERSION);
 
         /**
          * Google jwt client instance
@@ -26,19 +74,38 @@ class AnalyticsReportLoader {
 
     /**
      * Init
+     * @param {{
+     *     isSilent: boolean
+     * }} options
      * @public
      */
-    init() {
+    init(options) {
+        if (options.isQuiet) {
+            sequelize.options.logging = false;
+        }
         try {
-            this.authorize_();
-            this.loadReport_();
+            await(this.authorize_());
         } catch (error) {
-            console.log(error);
+            logger.error(error);
         }
     }
 
     /**
+     * Get data from google analytics api and write it to db
+     * @public
+     */
+    updateCourseAnalytics() {
+        let data = await(this.loadReport_());
+        let headers = this.initHeaders_(data.columnHeaders);
+        let coursesAnalytics = this.getCoursesAnalytics_(
+            data.rows, headers
+        );
+        this.fillDb_(coursesAnalytics);
+    }
+
+    /**
      * Authorize with given credentials from key file
+     * @return {Promise}
      * @private
      */
     authorize_() {
@@ -49,42 +116,140 @@ class AnalyticsReportLoader {
             ['https://www.googleapis.com/auth/analytics.readonly'],
             null
         );
-
-        this.jwt_.authorize(function(error) {
-            if (error) {
-                console.log(error);
-                throw new Error('Cant authorize');
-            }
+        return new Promise((resolve, reject) => {
+            this.jwt_.authorize((error, responce) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(responce);
+                }
+            });
         });
     }
 
+
     /**
-     * Load report
+     * Load report from google analytics api
+     * @return {Promise}
+     * @private
      */
     loadReport_() {
-        this.analyticsApi_.data.ga.get({
-            'auth': this.jwt_,
-            'ids': VIEW_ID,
-            'end-date': 'today',
-            'start-date': '14daysAgo',
-            'filters': 'ga:productListName==search results',
-            'dimensions': 'ga:productName,ga:productSku',
-            'sort': '-ga:productListCTR',
-            'metrics': 'ga:productListCTR,ga:productListClicks',
-            'max-results': 500
-        }, function(err, responce) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(JSON.stringify(responce, null, 4));
-            }
+        let metrics =
+            `${PARAMS.ctr.queryName},` +
+            `${PARAMS.clicks.queryName},` +
+            `${PARAMS.views.queryName}`;
+        let dimensions = `${PARAMS.productName.queryName},` +
+            `${PARAMS.category.queryName}`;
+
+        return new Promise((resolve, reject) => {
+            this.analyticsApi_.data.ga.get({
+                'auth': this.jwt_,
+                'ids': VIEW_ID,
+                'end-date': END_DATE,
+                'start-date': START_DATE,
+                'filters': `${LIST_NAME_DIMENTION}==${LIST_NAME_VALUE}`,
+                'dimensions': dimensions,
+                'sort': `-${PARAMS.ctr.queryName}`,
+                'metrics': metrics,
+                'max-results': MAX_RESULTS
+            }, function(error, responce) {
+                if (error) {
+                    logger.error(error);
+                    reject(error);
+                } else {
+                    resolve(responce);
+                }
+            });
         });
+    }
+
+
+    /**
+     * Init header positions
+     * @param {Array<{
+     *     name: string
+     * }>} headers
+     * @return {Array<string>}
+     * @private
+     */
+    initHeaders_(headers) {
+        return headers.map((header, index) => {
+            let param = lodash.find(PARAMS, (paramObject) => {
+                return header.name == paramObject.queryName;
+            });
+
+            return param.name;
+        });
+    }
+
+
+    /**
+     * Get analytics data from given rows with given headers
+     * @param {Array<Array<string>>} rows
+     * @param {Array<string>} headers
+     * @return {Array<{
+     *     name: string,
+     *     ctr: number,
+     *     clicks: number,
+     *     views: number,
+     *     category: string
+     * }>}
+     * @private
+     */
+    getCoursesAnalytics_(rows, headers) {
+        let callback = lodash.partial(this.getRowAnalytics_, headers);
+        return rows.map(callback);
+    }
+
+    /**
+     * Transform row to course object
+     * @param {Array<string>} headers
+     * @param {Array<string>} row
+     * @return {{
+     *     name: string,
+     *     ctr: number,
+     *     clicks: number,
+     *     views: number,
+     *     category: string
+     * }}
+     * @private
+     */
+    getRowAnalytics_(headers, row) {
+        return row.reduce((previous, current, index) => {
+            let fieldName = headers[index];
+            previous[fieldName] = current;
+
+            return previous;
+        }, {});
+    }
+
+
+    /**
+     * Fill db with given data
+     * @param {Array<{
+     *     name: string,
+     *     ctr: number,
+     *     clicks: number,
+     *     views: number,
+     *     category: string
+     * }>} data
+     * @private
+     */
+    fillDb_(data) {
+        data.forEach(function(courseData) {
+            let course = await(services.course.getGetByNameAndCategoryAlias(
+                courseData.category, courseData.name
+            ));
+            if (course) {
+                services.course.updateCtr({
+                    courseId: course.id,
+                    ctr: courseData.ctr,
+                    clicks: courseData.clicks,
+                    views: courseData.views
+                });
+            }
+        }, this);
     }
 };
 
 module.exports = AnalyticsReportLoader;
-
-if (!module.parent) {
-    let analyticsReportLoader = new AnalyticsReportLoader();
-    module.exports = analyticsReportLoader.init();
-}
