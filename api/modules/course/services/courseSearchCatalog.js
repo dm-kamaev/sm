9ce249b54aba26f66407712033eebf3995c6cc9d
Dmitrii K <dm-kamaev@rambler.ru>
2016-11-25 17:58:08 +0300

    BP-1767: added rest api for course search catalog;

---
+++ api/modules/course/services/courseSearchCatalog.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// author: dm-kamaev
+// work with table db 'courses_search_catalog'
+
+const async = require('asyncawait/async'),
+    await = require('asyncawait/await');
+
+const models = require('../../../../app/components/models').all;
+
+let service = {
+    name: 'courseSearchCatalog'
+};
+
+
+/**
+ * get all
+ * @return {Object[]} all records
+ */
+service.getAll = async(function() {
+    return await(models.CourseSearchCatalog.findAll());
+});
+
+
+/**
+ * get by id
+ * @param  {Number} linkId
+ * @return {Object{}} one record
+ */
+service.getById = async(function(linkId) {
+    return await(models.CourseSearchCatalog.findOne({
+        where: {
+            id: linkId
+        }
+    }));
+});
+
+
+/**
+ * create
+ * @param  {Object} data {
+ *
+ * }
+ * @return {[type]}       [description]
+ */
+service.create = async(function(data) {
+    await(models.CourseSearchCatalog.create(data));
+});
+
+
+/**
+ * update
+ * @param  {Number} linkId id record
+ * @param  {Object{}} data
+ */
+service.update = async(function(linkId, data) {
+    await(models.CourseSearchCatalog.update(data, {
+        where: {
+            id: linkId
+        }
+    }));
+});
+
+
+/**
+ * delete
+ * @param  {Number} linkId record id
+ */
+service.delete = async(function(linkId) {
+    await(models.CourseSearchCatalog.destroy({
+        where: {
+            id: linkId
+        }
+    }));
+});
+
+module.exports = service;
