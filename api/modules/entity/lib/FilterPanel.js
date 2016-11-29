88b16d058b1ba95e5ebabea454a326bf8c852f6e
Alexander Nedashkovskiy <texnik88@gmail.com>
2016-11-29 12:40:31 +0300

    BP-1787: move filter view type to enum, small filter panel refactoring

--- api/modules/entity/lib/FilterPanel.js
+++ api/modules/entity/lib/FilterPanel.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const lodash = require('lodash');
+
+const filterType = require('../enums/filterViewType');
+
 class FilterPanel {
 
     /**
@@ -174,7 +178,7 @@ class FilterPanel {
     setFilterInput(filterParams, opt_checkedValues) {
         var params = filterParams;
 
-        params.config.type = 'input';
+        params.config.type = filterType.INPUT;
 
         this.setFilter(params, opt_checkedValues);
 
@@ -194,7 +198,7 @@ class FilterPanel {
     setFilterSwitch(filterParams, opt_checkedValues) {
         var params = filterParams;
 
-        params.config.type = 'switch';
+        params.config.type = filterType.SWITCH;
 
         this.setFilter(params, opt_checkedValues);
 
@@ -214,7 +218,7 @@ class FilterPanel {
     setFilterLabels(filterParams, opt_checkedValues) {
         var params = filterParams;
 
-        params.config.type = 'labels';
+        params.config.type = filterType.LABELS;
 
         this.setFilter(params, opt_checkedValues);
 
@@ -320,7 +324,7 @@ class FilterPanel {
      *         header: ?Object,
      *         options: Array<Object>
      *     },
-     *     congig: Object
+     *     config: Object
      * }} filterParams
      * @param {Array<(number|string)>|undefined} checkedValues
      * @return {Object}
@@ -368,6 +372,8 @@ class FilterPanel {
 
     /**
      * Set values for options in order
+     * It uses lodash.map as it can iterate over Arrays which create from Array
+     * constructor with defined length
      * @param {Array<{
      *     label: string,
      *     isChecked: (boolean|undefined)
@@ -380,9 +386,10 @@ class FilterPanel {
      * @protected
      */
     createOptionsValuesInOrder(options) {
-        return options.map((option, index) => {
-            option.value = index;
-            return option;
+        return lodash.map(options, (option, index) => {
+            let result = option || {};
+            result.value = index;
+            return result;
         });
     }
 
