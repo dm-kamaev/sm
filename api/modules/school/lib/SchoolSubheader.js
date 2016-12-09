4554b65c10a4198dc5de5d06e55db9a963d3c96c
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-12-09 13:12:45 +0300

    BP-1850: added class to getting subheader data for schools

---
+++ api/modules/school/lib/SchoolSubheader.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Subheader = require('../../entity/lib/Subheader');
+
+const entityType = require('../../entity/enums/entityType');
+
+class SchoolSubheader extends Subheader {
+
+    /**
+     * Init params for subheader
+     */
+    constructor() {
+        super();
+
+
+        /**
+         * Data for logo
+         * @type {Object<string, string>}
+         * @protected
+         * @override
+         */
+        this.logo = {
+            altText: '«Школы Мела»',
+            imgUrl: '/static/images/n-common/b-sm-subheader/school-logo.svg'
+        };
+
+
+        /**
+         * Links data
+         * @type {Object<string, string>}
+         * @protected
+         * @override
+         */
+        this.links = {
+            nameL: 'Все школы Москвы',
+            nameM: 'Все школы',
+            url: '/school'
+        };
+
+
+        /**
+         * Data for search
+         * @type {Object<string, string>}
+         * @protected
+         * @override
+         */
+        this.search = {
+            placeholder: 'Район, метро, номер школы',
+            pageAlias: 'school'
+        };
+    }
+
+
+    /**
+     * Config setter
+     * @param {boolean} isBottomLine
+     * @override
+     * @public
+     */
+    setConfig(isBottomLine) {
+        super.setConfig(isBottomLine);
+        this.params.config.entityType = entityType.SCHOOL;
+    }
+}
+
+module.exports = SchoolSubheader;
