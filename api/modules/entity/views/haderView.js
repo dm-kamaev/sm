6b29bf9aee2a350873cb5ea83f8d9203788dfbe6
Alexander Nedashkovskiy <texnik88@gmail.com>
2016-12-12 17:35:23 +0300

    BP-1846: add common view for header

---
+++ api/modules/entity/views/haderView.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const entityType = require('../enums/entityType');
+
+const Header = require('../lib/Header');
+
+const headerView = {};
+
+/**
+ * Header view
+ * @param {{
+ *     entityType: string,
+ *     protocol: string,
+ *     coursesHost: string,
+ *     schoolsHost: string
+ * }} data
+ * @return {Object<string, (string|Object)>}
+ */
+headerView.render = function(config, entityType) {
+    let header = new Header();
+
+    header.init({
+        entityType: entityType,
+        entityLinks: headerView.links(config)
+    });
+
+    return header.getParams();
+};
+
+
+/**
+ * Generate links for header menu
+ * @param {{
+ *     protocol: string
+ *     courses: {
+ *         host: string
+ *     },
+ *     schools: {
+ *         host: string
+ *     }
+ * }} data
+ * @return {Object<string, string>}
+ */
+headerView.links = function(config) {
+    let protocol = config.protocol;
+    return {
+        [entityType.SCHOOL]: `${protocol}://${config.schools.host}`,
+        [entityType.COURSE]: `${protocol}://${config.courses.host}`
+    };
+};
+
+module.exports = headerView;
