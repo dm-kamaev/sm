7ffdf13348d245c4257662dedec25b33303134b8
Viktoriya Bilyk <viktoriya566@rambler.ru>
2016-12-16 11:25:13 +0300

    BP-1899: created footerView

---
+++ api/modules/entity/views/footerView.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const footerView = {};
+
+/**
+ * Footer view
+ * @param {Array<{
+ *     content: string,
+ *     url: string
+ * }>} seoLinks
+ * @return {Object<string, (string|Array<Object<string, string>>)>}
+ */
+footerView.render = function(seoLinks) {
+    let year = new Date().getFullYear();
+
+    return {
+        copyright: '© Маркет Мела ' + year,
+        contactLinks: footerView.contactLinks(),
+        seoLinks: seoLinks
+    };
+};
+
+
+/**
+ * Generate contact links
+ * @return {Array<Object<string, string>>}
+ */
+footerView.contactLinks = function() {
+    return [{
+        'content': 'Сотрудничество',
+        'url': 'mailto:vs@mel.fm'
+    }, {
+        'content': 'Пользовательское соглашение',
+        'url': 'http://mel.fm/terms-of-use'
+    }];
+};
+
+module.exports = footerView;
