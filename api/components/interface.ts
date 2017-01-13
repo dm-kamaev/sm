1eea96041639fbf84eab42ed5c7e5db31ee6aa6f
Alexander Nedashkovskiy <texnik88@gmail.com>
2017-01-13 13:46:00 +0300

    BP-1913 remove unnecessary interface

--- api/components/interface.ts
+++ api/components/interface.ts
@@ -3,19 +3,13 @@
  */
 export interface ServiceException {
     new(message: string): ServiceException;
-};
+}
 
 export interface ControllerError {
     new(exception: ServiceException): ControllerError;
-};
-
-export interface ActiveRecord {
-    update(data: any): ActiveRecord;
-    destroy(): ActiveRecord;
-    toJSON(): any;
-};
+}
 
 export interface LegacyController {
     errors: Object;
     new(): LegacyController
-};
+}
