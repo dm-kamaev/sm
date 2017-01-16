4c1ba16bb50fe66f1f70c29f5e8de3efca6e99ec
Dmitrii K <dm-kamaev@rambler.ru>
2017-01-16 12:39:56 +0300

    BP-1916: global handler for error

--- app/middleware/criticalErrorHandler.js
+++ app/middleware/criticalErrorHandler.js
@@ -4,17 +4,41 @@ const logger = require('../components/logger/logger').getLogger('app');
 const ApiError = require('nodules/controller/ControllerError');
 
 module.exports = function(err, req, res, next) {
-    if (!(err instanceof ApiError)) {
-        logger.critical(err);
+    let result;
+    logger.critical(err);
+    if (err instanceof ApiError) {
+        res.status(err.status);
+        result = [{
+            code: err.code,
+            message: err.message
+        }];
+        res.send(result);
+        res.end();
+    } else if (/^Sequelize/.test(err.name)) {
+        logger.critical(JSON.stringify(err, null, 2));
+        res.status(422);
+        result = [{
+            code: 'ValidationError',
+            validationErrors: err.errors
+        }];
+        res.send(result);
+        res.end();
+    } else {
         if (/(\/error)$/.test(req.path)) {
-            res.status(500).end('Internal Server Error');
+            res.status(500);
+            result = [{
+                code: 'InternalServerError',
+                message: err.message
+            }];
+            res.send(result);
+            res.end();
         } else {
             res.redirect('/error');
         }
-    } else {
-        logger.debug(err);
-        res.status(err.status);
-        res.send(err.message);
-        res.end();
     }
 };
+
+
+
+
+
