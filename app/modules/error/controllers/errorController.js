bbfd5eb26f5df24ee76fe08dbcd34d8c75c45b7f
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-01-24 13:58:03 +0300

    BP-2075: replaced redirect on l-school-error-not-found to common l-error-not-found

--- app/modules/error/controllers/errorController.js
+++ app/modules/error/controllers/errorController.js
@@ -2,91 +2,22 @@
 
 const soy = require.main.require('./app/components/soy');
 const services = require('../../../../app/components/services').all;
-const schoolView = require('../../../../api/modules/school/views/schoolView');
-
-const entityTypeEnum =
-    require('../../../../api/modules/entity/enums/entityType');
-const errorView = require('../../../../api/modules/error/views/errorView'),
-    schoolErrorView =
-        require('../../../../api/modules/error/views/schoolErrorView');
-
 const logger = require('../../../components/logger/logger').getLogger('app');
 
-const config = require('../../../config').config;
-const analyticsId = config.schools.analyticsId;
-const yandexMetrikaId = config.schools.yandexMetrikaId;
+const contentExperiment =
+        require('../../../components/contentExperiment/contentExperiment');
 
-const MODIFIER = 'stendhal',
-    FB_CLIENT_ID = config.facebookClientId,
+const errorView = require('../../../../api/modules/error/views/errorView');
+
+const config = require('../../../config').config;
+
+const FB_CLIENT_ID = config.facebookClientId,
     CARROTQUEST_ID = config.carrotquestId;
 
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
 let controller = {};
-
-controller.schoolNotFound = async(function(req, res) {
-    var user = req.user || {};
-
-    var dataPromises = {
-        popularSchools: services.school.getRandomPopularSchools(5),
-        amountSchools: services.school.getSchoolsCount(),
-        authSocialLinks: services.auth.getAuthSocialUrl(),
-        favorites: [],
-        seoLinks: services.seoSchoolList.getByTypes()
-    };
-
-    var data = await(dataPromises);
-
-    let errorText;
-
-    if (/(\/error)$/.test(req.path)) {
-        res.status(500);
-        errorText = 'Что-то пошло не так';
-    } else {
-        res.status(404);
-    }
-
-    var popularAliases = await(services.page.getAliases(
-        data.popularSchools.map(school => school.id),
-        entityTypeEnum.SCHOOL
-    ));
-    data.popularSchools = schoolView.joinAliases(
-        data.popularSchools,
-        popularAliases
-    );
-
-    let templateData = schoolErrorView.render({
-        user: user,
-        favorites: data.favorites,
-        authSocialLinks: data.authSocialLinks,
-        seoLinks: data.seoLinks,
-        entityType: entityTypeEnum.SCHOOL,
-        config: config
-    });
-
-    console.log(JSON.stringify(templateData.sideMenu, null, 4));
-
-    var html = soy.render('sm.lErrorSchoolNotFound.Template.base', {
-        params: {
-            data: templateData,
-            errorText: errorText || 'Страница, которую вы искали, не найдена',
-            popularSchools: schoolView.popular(data.popularSchools),
-            dataLinks: schoolView.dataLinks(),
-            amountSchools: data.amountSchools,
-            config: {
-                staticVersion: config.lastBuildTimestamp,
-                analyticsId: analyticsId,
-                yandexMetrikaId: yandexMetrikaId,
-                carrotquestId: CARROTQUEST_ID,
-                csrf: req.csrfToken()
-            }
-        }
-    });
-    res.header('Content-Type', 'text/html; charset=utf-8');
-    res.end(html);
-});
-
 
 controller.generalError = async(function(
     req, res, next, entityType, subdomain
@@ -95,6 +26,8 @@ controller.generalError = async(function(
     try {
         let authSocialLinks = services.auth.getAuthSocialUrl(),
             user = req.user || {};
+
+        let factory = contentExperiment.getFactoryByQuery(req.query);
 
         let data = await({
             favorites: services.favorite.getFavoriteEntities(user.id)
@@ -125,8 +58,9 @@ controller.generalError = async(function(
                     page: 'error-not-found',
                     staticVersion: config.lastBuildTimestamp,
                     entityType: entityType,
-                    modifier: MODIFIER,
+                    modifier: factory,
                     analyticsId: config[subdomain].analyticsId,
+                    experimentId: config[subdomain].experimentId,
                     yandexMetrikaId: config[subdomain].yandexMetrikaId,
                     carrotquestId: CARROTQUEST_ID,
                     csrf: req.csrfToken(),
