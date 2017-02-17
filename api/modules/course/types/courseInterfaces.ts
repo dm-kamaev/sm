3fda25b1fe89062f8c74b960eb08248222a3c4cb
Alexander Nedashkovskiy <texnik88@gmail.com>
2017-02-17 20:25:20 +0300

    BP-2177 types moved to divided folder

---
+++ api/modules/course/types/courseInterfaces.ts
@@ -0,0 +1,48 @@
+/**
+ * @fileOverview Type for course model
+ */
+import * as Sequelize from 'sequelize/v3';
+
+import {
+    PageMetaInformationAttributes,
+    PageMetaInformationInstance
+} from '../../entity/types/pageMetaInterfaces';
+
+
+export interface CourseAttributes {
+    id?: number;
+    name?: string;
+    brandId: number;
+    type: string;
+    description: string;
+    fullDescription: string;
+    about: string;
+    entranceExam: string;
+    learningOutcome: string;
+    leadType: string;
+    score: Array<number>;
+    scoreCount: Array<number>;
+    totalScore?: number;
+    imageUrl?: string;
+    embedId?: string;
+    ctr: number;
+}
+
+export interface CourseInstance
+    extends Sequelize.Instance<CourseAttributes>, CourseAttributes {
+        addPageMetaInformation:
+            Sequelize.BelongsToManyAddAssociationsMixin<
+                PageMetaInformationInstance,
+                number,
+                PageMetaInformationAttributes
+            >;
+        getPageMetaInfromations:
+            Sequelize.BelongsToManyGetAssociationsMixin<
+                PageMetaInformationInstance
+            >;
+        hasPageMetaInformation:
+            Sequelize.BelongsToManyHasAssociationMixin<
+                PageMetaInformationInstance,
+                number
+            >;
+}
