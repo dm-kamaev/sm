63038444c372b0d7f61aa5d04dc3e0164bf298ae
Alexander Nedashkovskiy <texnik88@gmail.com>
2017-03-03 13:00:17 +0300

    BP-2195 university commment admin controller

---
+++ api/modules/comment/views/universityCommentView.ts
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview View for university comment
+ */
+
+import {
+    UniversityCommentInstance,
+    AdminUniversityComment
+} from '../types/universityComment';
+
+
+class UniversityCommentView {
+    public adminRender(): AdminUniversityComment {
+
+    }
+}
