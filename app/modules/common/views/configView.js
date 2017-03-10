9a3ddc187b88834cbc584db0c346a34124da8416
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-03-10 16:58:05 +0300

    BP-2226: fix template params, fix view for config params

--- app/modules/common/views/configView.js
+++ app/modules/common/views/configView.js
@@ -27,7 +27,6 @@ let configView = {
  *     analyticsId: string,
  *     experimentId: ?string,
  *     yandexMetrikaId: number,
- *     carrotquestId: string,
  *     domain: string,
  *     fbClientId: ?number,
  *     csrf: string
@@ -51,7 +50,6 @@ configView.render = function(params) {
         analyticsId: config[subdomain].analyticsId,
         experimentId: config[subdomain].experimentId,
         yandexMetrikaId: config[subdomain].yandexMetrikaId,
-        carrotquestId: config.carrotquestId,
         fbClientId: config.facebookClientId,
         domain: config[subdomain].host,
         csrf: params.csrf
