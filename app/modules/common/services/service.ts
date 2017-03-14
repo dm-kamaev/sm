526571fcb3988a45e81f51410a2ec6d567a79eb3
dsavkin <dmitrsavk@yandex.ru>
2017-03-14 15:11:05 +0300

    BP-2225 add abstract service

---
+++ app/modules/common/services/service.ts
@@ -0,0 +1,53 @@
+import * as axios from 'axios';
+
+const config = require('../../../config/admin');
+
+const headers = {
+    [config.headers.token.name]: config.headers.token.value
+};
+
+abstract class Service {
+
+    private axios: Axios.AxiosInstance;
+
+    abstract handleError(error: any): void;
+
+    constructor() {
+        this.axios = axios.create({
+            headers
+        });
+    }
+
+    public async post(
+        url: string,
+        data?: any,
+        config?: Axios.AxiosXHRConfigBase<any>
+    ): Promise<Axios.AxiosXHR<any>> {
+        let res: Axios.AxiosXHR<any>;
+
+        try {
+            res = await(this.axios.post(url, data, config));
+        } catch (error) {
+            this.handleError(error);
+        }
+
+        return res;
+    }
+
+    public async get(
+        url: string,
+        config?: Axios.AxiosXHRConfigBase<any>
+    ): Promise<Axios.AxiosXHR<any>> {
+        let res: Axios.AxiosXHR<any>;
+
+        try {
+            res = await(this.axios.get(url, config));
+        } catch (error) {
+            this.handleError(error);
+        }
+
+        return res;
+    }
+}
+
+export {Service};
