d29195b67d82366b361e4100fd3a74d187dfe065
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-03-14 14:04:02 +0300

    BP-2228: add params for l-university

---
+++ app/blocks/n-university/l-university/params.d.ts
@@ -0,0 +1,55 @@
+import {
+    bSmSubheader
+} from '../../n-common/b-sm-subheader/params';
+
+import {iLayoutStendhal} from '../../n-clobl/i-layout/params';
+
+export namespace lUniversity {
+    /*
+     * sm.lUniversity.Params
+     */
+    export namespace Params {
+        /*
+         * sm.lUniversity.Params.Data
+         */
+        export interface Data {
+            openGraph: (any|undefined),
+            seo: {
+                metaTitle: string,
+                metaDescription: (string|undefined)
+            },
+            subHeader: bSmSubheader.Params.Data,
+            header: any,
+            sideMenu: any,
+            user?: {
+                firstName: (string|undefined),
+                lastName: (string|undefined)
+            },
+            authSocialLinks: {
+                vk: (string|undefined),
+                fb: (string|undefined)
+            },
+            entityData: any,
+            subscribeBoard: any,
+            navigationPanel: any,
+            modalComment: any,
+            footer: any
+        }
+
+        /*
+         * sm.lUniversity.Params.Config
+         */
+        export interface Config extends iLayoutStendhal.Params.Config {
+        }
+    }
+
+
+    /*
+     * sm.lUniversity.Params
+     */
+    export interface Params {
+        data: Params.Data,
+        config: Params.Config
+    }
+}
+
