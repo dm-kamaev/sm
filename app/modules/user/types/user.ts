04174f7e3e835c0ac5ef55a338426d9d52e04f86
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-03-14 14:06:34 +0300

    BP-2228: add user view for layout views

---
+++ app/modules/user/types/user.ts
@@ -0,0 +1,23 @@
+export type BackendUser = {
+    id: number;
+    facebookId: string;
+    vkId: string;
+    okId: string;
+    googleId: string;
+    twitterId: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    phone: string;
+    email: string;
+    photoUrl: string;
+    status: string;
+    birthDate: string;
+    created_at: Date;
+    updated_at: Date;
+};
+
+export interface UserData {
+    firstName: string;
+    lastName: string;
+}
