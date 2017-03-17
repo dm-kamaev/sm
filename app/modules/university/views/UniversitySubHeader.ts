a681dc8f09c7048a878bfef955331a8cf722949c
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-03-17 14:04:41 +0300

    BP-2299: add links data for university subheader

--- app/modules/university/views/UniversitySubHeader.ts
+++ app/modules/university/views/UniversitySubHeader.ts
@@ -1,4 +1,4 @@
-import {SubHeader} from '../../common/lib/SubHeader';
+import {SubHeader, Data} from '../../common/lib/SubHeader';
 
 import {entityType} from '../../common/enums/entityType';
 
@@ -14,25 +14,74 @@ class UniversitySubHeader extends SubHeader {
             imgUrl: '/static/images/n-common/b-sm-subheader/university-logo.svg'
         };
 
-        this.listLinks = {
-            opener: 'Все ВУЗы',
-            content: {
-                items: []
-            }
-        };
-
-        this.links = {
+        this.link = {
             nameL: 'Подобрать ВУЗ',
             nameM: 'Подобрать ВУЗ',
             url: '/search',
             theme: 'neptune'
         };
 
+        this.rowLinks = {
+            linkConfig: {
+                theme: 'neptune',
+                size: 'xxl'
+            },
+            listConfig: {}
+        };
+
+        this.dropdownLinks = {
+            data: {
+                content: {
+                    items: [],
+                    itemConfig: {
+                        theme: 'neptune',
+                        size: 'xxl'
+                    }
+                }
+            },
+            config: {
+                openerTheme: 'neptune',
+                openerSize: 'xxl',
+                iconType: 'icon-svg',
+                iconName: 'blue-arrow',
+            }
+        }
+
         this.search = {
             placeholder: 'ВУЗ, специальность',
             pageAlias: 'search',
             redirect: false
         };
+    }
+
+    protected setLinks(listLinks) {
+        let links = [
+            {
+                url: '/',
+                label: 'Программы обучения',
+                isSelected: true
+            }, {
+                url: '/',
+                label: 'Профессия'
+            }, {
+                url: '/',
+                label: 'ВУЗ'
+            }
+        ];
+
+        super.setLinks(links);
+
+        if (links) {
+            this.setRowLinks(links);
+        }
+    }
+
+    protected setDropdownLinks(links) {
+        let link = links.find(link => link.isSelected);
+
+        this.dropdownLinks.data.opener = link.label;
+
+        super.setDropdownLinks(links);
     }
 };
 
