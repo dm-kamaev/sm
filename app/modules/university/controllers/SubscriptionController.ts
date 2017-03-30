3aacd302b95f0091e21fd935b0cafd6d473e6b83
dsavkin <dmitrsavk@yandex.ru>
2017-03-30 22:55:41 +0300

    BP-2383 change subscribe logic

--- app/modules/university/controllers/SubscriptionController.ts
+++ app/modules/university/controllers/SubscriptionController.ts
@@ -3,32 +3,41 @@ import {service} from '../services/subscriptionService';
 
 const Controller: LegacyController = require('nodules/controller').Controller;
 
-import {EmailAlreadyExist as EmailAlreadyExistError}
-    from './errors/EmailAlreadyExist';
-
 import {InvalidEmail as InvalidEmailError}
     from './errors/InvalidEmail';
+
+import {MailChimpErrorTitle} from '../constants/MailChimpErrorTitle';
 
 class SubscriptionController extends Controller {
     constructor() {
         super();
 
         this.errors = {
-            EmailAlreadyExistException: EmailAlreadyExistError,
             InvalidEmailException: InvalidEmailError
         };
     }
 
 
     /**
+     * @apiDefine InvalidEmailError
+     * @apiError (422) InvalidEmailError Invalid email
+     * @apiErrorExample {json} Error-Response:
+     *      HTTP/1.1 422 Unprocessable Entity
+     *      [{
+     *           "code": "InvalidEmail",
+     *           "message": "Некорректный email"
+     *      }]
+     */
+
+    /**
      * @api {post} /program/subscribe Add subscriber
      * @apiVersion 1.0.0
      * @apiName addSubscriber
      *
-     * @apiSuccess {Object} subsciber
-     * @apiSuccess {Number} subsciber.id
-     * @apiSuccess {String} subsciber.email
-     * @apiSuccess {String} subsciber.status
+     * @apiSuccess {Object} subscriber
+     * @apiSuccess {Number} subscriber.id
+     * @apiSuccess {String} subscriber.email
+     * @apiSuccess {String} subscriber.status
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -38,17 +47,30 @@ class SubscriptionController extends Controller {
      *         "status": "subscribed"
      *     }
      *
-     * @apiError (400) EmailAlreadyExistError Email already exist.
-     * @apiError (422) EmailAlreadyExistError Email is invalid.
+     * @apiUse InvalidEmailError
      */
     public async actionCreate(actionContext: any) {
-        const result = await(service.create(
-            actionContext.data.id,
-            actionContext.data.email
-        ));
+        const id = actionContext.data.id;
+        const email = actionContext.data.email;
+
+        let result = await service.create(id, email);
+
+        if (result === MailChimpErrorTitle.EMAIL_EXIST) {
+            const subscribers = await service.getList();
+
+            const subscriber = subscribers.data.members
+                .find(subscriber => subscriber['email_address'] === email);
+
+            subscriber['merge_fields']['PROGRAM_LI'] += `, ${id}`;
+
+            result = await service.update(
+                subscriber.id,
+                subscriber['merge_fields']['PROGRAM_LI']
+            );
+        }
 
         return {
-            id: result.data['merge_fields']['PROGRAM_ID'],
+            id: result.data.id,
             email: result.data['email_address'],
             status: result.data.status
         };
