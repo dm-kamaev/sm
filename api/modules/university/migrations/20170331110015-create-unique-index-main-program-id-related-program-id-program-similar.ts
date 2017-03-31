55282e17c354bef05510ab7172cc67df2ae4ceb4
Dmitrii K <dm-kamaev@rambler.ru>
2017-03-31 10:53:31 +0300

    BP-2196: created table program_similar

---
+++ api/modules/university/migrations/20170331110015-create-unique-index-main-program-id-related-program-id-program-similar.ts
@@ -0,0 +1,15 @@
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.sequelize.query(`
+            CREATE UNIQUE INDEX
+                main_program_id_related_program_id_uidx
+            ON program_similar
+                (main_program_id, related_program_id);
+        `);
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.sequelize.query(`
+            DROP INDEX main_program_id_related_program_id_uidx
+        `);
+    }
+};
