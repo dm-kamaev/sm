d640516c89854b3c83f941f124baeb1ac6d748b7
Alexander Nedashkovskiy <texnik88@gmail.com>
2017-04-12 20:06:56 +0300

    BP-2425 program view

---
+++ api/modules/university/views/program.ts
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview View for program
+ */
+import {
+    ProgramAttribute,
+    ProgramUrl,
+    ProgramSimilar
+} from '../types/program';
+
+class ProgramView {
+    renderSimilar(
+            programs: ProgramAttribute[], urls: ProgramUrl[]
+    ): ProgramSimilar[] {
+        return programs.map(program => {
+            const url = urls.find(url => url.id === program.id).url;
+
+            return {
+                id: program.id,
+                name: program.name,
+                url: url
+            };
+        });
+    }
+}
+
+export const programView = new ProgramView();
