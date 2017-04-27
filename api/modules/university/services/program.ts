78ed25bc6d26de59866a096c12a089ed5302d010
Dmitrii K <dm-kamaev@rambler.ru>
2017-04-27 11:09:30 +0300

    BP-2196: fix bug with undefined statistics and add equality cities

--- api/modules/university/services/program.ts
+++ api/modules/university/services/program.ts
@@ -10,6 +10,9 @@ import {Model as ProgramMajorModel} from '../models/ProgramMajor';
 import {
     Model as ProgramPageMetaModel
 } from '../models/ProgramPageMetaInformation';
+import {
+    Model as UniversityModel
+} from '../models/University';
 import {Model as CommentGroupModel} from '../../comment/models/commentGroup';
 import {
     Model as ProgramCommentModel
@@ -232,6 +235,10 @@ class ProgramService {
             }, {
                 model: ProgramEgeExamModel,
                 as: 'programEgeExams'
+            }, {
+                model: UniversityModel,
+                as: 'university',
+                attributes: ['cityId']
             }],
         });
         return programs;
