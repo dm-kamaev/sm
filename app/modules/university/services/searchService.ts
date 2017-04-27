652d92dc6b7f26e47cca1309fef5a194dbc601b4
Alexander Nedashkovskiy <texnik88@gmail.com>
2017-04-27 18:03:40 +0300

    BP-2303 add types and search params transformation in search service

--- app/modules/university/services/searchService.ts
+++ app/modules/university/services/searchService.ts
@@ -1,6 +1,14 @@
 const logger = require('../../../components/logger/logger').getLogger('app');
 
+<<<<<<< 6f11d6ea8c06b0113ba2e6d9e5869f15e2174760
 import {QueryParams} from '../types/programSearch';
+=======
+import {
+    SearchParams,
+    BackendEgeSearchParam,
+    BackendSearchParams
+} from '../types/search';
+>>>>>>> BP-2303 add types and search params transformation in search service
 
 import {Service, RequestParams} from '../../common/services/Service';
 import {SuggestProgram, BackendProgramResults} from '../types/program';
@@ -26,11 +34,11 @@ class SearchService extends Service {
     }
 
     public async findByParams(
-            queryParams: QueryParams): Promise<BackendProgramResults> {
+            queryParams: SearchParams): Promise<BackendProgramResults> {
         const requestParams: RequestParams = {
             url: `${this.baseUrl}`,
             method: 'get',
-            params: queryParams
+            params: this.initParams_(queryParams)
         };
 
         const response = await this.send(requestParams);
@@ -50,6 +58,32 @@ class SearchService extends Service {
         logger.critical(error);
         throw error;
     }
+
+    private initParams_(queryParams: SearchParams): BackendSearchParams {
+        const ege: BackendEgeSearchParam[] = queryParams.egeSubjects.map(
+            (subjectId) => {
+                const subjectResult = queryParams.egeResults.find(
+                    (result) => result.subjectId == subjectId
+                );
+
+                return {
+                    [subjectId]: subjectResult.value || 100
+                };
+            }
+        );
+
+        return {
+            cities: queryParams.cities,
+            ege: ege,
+            features:queryParams.features,
+            majors: queryParams.majors,
+            maxPrice: queryParams.maxPrice,
+            payType: queryParams.payType,
+            page: queryParams.page,
+            sortType: queryParams.sortType,
+        };
+    }
 }
 
 export const searchService = new SearchService();
+
--- app/modules/university/types/programSearchLayout.ts
+++ app/modules/university/types/programSearchLayout.ts
@@ -4,7 +4,22 @@ import {BackendProgramResults} from './program';
 import {Subject} from './egeExam';
 import {BackendCity} from '../../geo/types/city';
 import {PopularMajor} from './major';
+<<<<<<< 6f11d6ea8c06b0113ba2e6d9e5869f15e2174760
 import {QueryParams} from './programSearch';
+=======
+
+export type QueryParams = {
+    cities?: Array<number>;
+    egeSubjects?: Array<number>;
+    payType?: Array<number>;
+    egeResults?: Array<string>;
+    maxPrice?: number;
+    majors?: string;
+    features?: string;
+    page?: number;
+    sortType?: number;
+};
+>>>>>>> BP-2303 add types and search params transformation in search service
 
 export type RenderParams = {
     data: BackendData;
