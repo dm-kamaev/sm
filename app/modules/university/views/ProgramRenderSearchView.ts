b6ffb0fbf5bbbed8aeff6c0c7b2eb95d33bb28ff
Viktoriya Bilyk <viktoriya566@rambler.ru>
2017-05-05 12:32:30 +0300

    BP-2553: change placeholder text in empty search results for universities

--- app/modules/university/views/ProgramRenderSearchView.ts
+++ app/modules/university/views/ProgramRenderSearchView.ts
@@ -148,6 +148,16 @@ class ProgramRenderSearchView extends LayoutView {
                 resultsList.universityCount
             ),
             title: 'Вузы Москвы',
+            placeholder: {
+                text: [
+                    'Ничего страшного! Во-первых, в&nbsp;нашей базе есть ' +
+                        'не&nbsp;все программы обучения, во-вторых, ' +
+                        'мы&nbsp;ориентируемся на&nbsp;проходные  баллы ' +
+                        'прошлого года, в&nbsp;этом году они могут ' +
+                        'быть другими. Попробуйте изменить параметры' +
+                        'поиска и&nbsp;попробовать ещё раз.'
+                ]
+            },
             description: this.seo.metaDescription,
             countResults: resultsList.programCount,
             entityList: {
